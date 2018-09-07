6a4f459cadc1db8552a152ee548fd743e334b5e4
Zihao Yu <yuzihao@ict.ac.cn>
2018-09-08 03:24:55 +0800

    device,vga: store screen size into I/O register
    * this can keep the software unchanged while changing the screen size

--- src/device/vga.c
+++ src/device/vga.c
@@ -3,10 +3,12 @@
 #ifdef HAS_IOE
 
 #include "device/mmio.h"
+#include "device/port-io.h"
 #include <SDL2/SDL.h>
 
 #define VMEM 0x40000
 
+#define SCREEN_PORT 0x100 // Note that this is not the standard
 #define SCREEN_H 300
 #define SCREEN_W 400
 
@@ -15,6 +17,7 @@ static SDL_Renderer *renderer;
 static SDL_Texture *texture;
 
 static uint32_t (*vmem) [SCREEN_W];
+static uint32_t *screensize_port_base;
 
 void update_screen() {
   SDL_UpdateTexture(texture, NULL, vmem, SCREEN_W * sizeof(vmem[0][0]));
@@ -30,6 +33,8 @@ void init_vga() {
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
 
+  screensize_port_base = add_pio_map(SCREEN_PORT, 4, NULL);
+  *screensize_port_base = ((SCREEN_W) << 16) | (SCREEN_H);
   vmem = add_mmio_map(VMEM, 0x80000, NULL);
 }
 #endif	/* HAS_IOE */
