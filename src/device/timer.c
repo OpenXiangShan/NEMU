780756b8def989ba7cb533a96af565225839fc63
Zihao Yu <yuzihao@ict.ac.cn>
2019-05-01 17:27:09 +0800

    device,timer: add us precision timer

--- src/device/timer.c
+++ src/device/timer.c
@@ -15,18 +15,20 @@ void timer_intr() {
 static uint32_t *rtc_port_base;
 
 void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0);
+  assert(offset == 0 || offset == 8 || offset == 12);
   if (!is_write) {
     struct timeval now;
     gettimeofday(&now, NULL);
     uint32_t seconds = now.tv_sec;
     uint32_t useconds = now.tv_usec;
     rtc_port_base[0] = seconds * 1000 + (useconds + 500) / 1000;
+    rtc_port_base[2] = useconds;
+    rtc_port_base[3] = seconds;
   }
 }
 
 void init_timer() {
-  rtc_port_base = (void*)new_space(4);
-  add_pio_map("rtc", RTC_PORT, (void *)rtc_port_base, 4, rtc_io_handler);
-  add_mmio_map("rtc", RTC_MMIO, (void *)rtc_port_base, 4, rtc_io_handler);
+  rtc_port_base = (void*)new_space(16);
+  add_pio_map("rtc", RTC_PORT, (void *)rtc_port_base, 16, rtc_io_handler);
+  add_mmio_map("rtc", RTC_MMIO, (void *)rtc_port_base, 16, rtc_io_handler);
 }
