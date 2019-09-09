196f4f95019f4023222a13c621a153de60de5c1e
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-09 23:33:43 +0800

    riscv64,diff-test: add attach

--- src/isa/riscv64/diff-test.c
+++ src/isa/riscv64/diff-test.c
@@ -2,14 +2,6 @@
 #include "monitor/diff-test.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
-  /*
-  int i;
-  for (i = 0; i < sizeof(cpu.gpr) / sizeof(cpu.gpr[0]); i ++) {
-    if (((uint64_t)cpu.gpr[i]._64) > ((uint64_t)0xffffffff)) {
-      printf("!!-0x%lx, 0x%lx\n", ref_r->gpr[i]._64, cpu.gpr[i]._64);
-    }
-  }*/
-  
   if (memcmp(&cpu, ref_r, DIFFTEST_REG_SIZE)) {
     int i;
     for (i = 0; i < sizeof(cpu.gpr) / sizeof(cpu.gpr[0]); i ++) {
@@ -22,4 +14,6 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 }
 
 void isa_difftest_attach(void) {
+  ref_difftest_memcpy_from_dut(0, guest_to_host(0), PMEM_SIZE);
+  ref_difftest_setregs(&cpu);
 }
