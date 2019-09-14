a90e1bef4c5abe6f41bf4038f9b815029965c518
Zihao Yu <yuzihao@ict.ac.cn>
2019-09-15 01:12:29 +0800

    riscv64,exec: put the table inside the table helper

--- src/isa/riscv64/exec/exec.c
+++ src/isa/riscv64/exec/exec.c
@@ -1,88 +1,67 @@
 #include "cpu/exec.h"
 #include "all-instr.h"
 
-static OpcodeEntry load_table [8] = {
-  EXW(lds, 1), EXW(lds, 2), EXW(lds, 4), EXW(ld, 8), EXW(ld, 1), EXW(ld, 2), EXW(ld, 4), EMPTY
-};
-
 static make_EHelper(load) {
-  decinfo.width = load_table[decinfo.isa.instr.funct3].width;
-  idex(pc, &load_table[decinfo.isa.instr.funct3]);
+  static OpcodeEntry table [8] = {
+    EXW(lds, 1), EXW(lds, 2), EXW(lds, 4), EXW(ld, 8), EXW(ld, 1), EXW(ld, 2), EXW(ld, 4), EMPTY
+  };
+  decinfo.width = table[decinfo.isa.instr.funct3].width;
+  idex(pc, &table[decinfo.isa.instr.funct3]);
 }
-
-static OpcodeEntry store_table [8] = {
-  EXW(st, 1), EXW(st, 2), EXW(st, 4), EXW(st, 8), EMPTY, EMPTY, EMPTY, EMPTY
-};
 
 static make_EHelper(store) {
-  decinfo.width = store_table[decinfo.isa.instr.funct3].width;
-  idex(pc, &store_table[decinfo.isa.instr.funct3]);
+  static OpcodeEntry table [8] = {
+    EXW(st, 1), EXW(st, 2), EXW(st, 4), EXW(st, 8), EMPTY, EMPTY, EMPTY, EMPTY
+  };
+  decinfo.width = table[decinfo.isa.instr.funct3].width;
+  idex(pc, &table[decinfo.isa.instr.funct3]);
 }
-
-static OpcodeEntry op_imm_table [8] = {
-  EX(add), EX(sll), EX(slt), EX(sltu), EX(xor), EX(srl), EX(or), EX(and)
-};
 
 static make_EHelper(op_imm) {
-  idex(pc, &op_imm_table[decinfo.isa.instr.funct3]);
+  static OpcodeEntry table [8] = {
+    EX(add), EX(sll), EX(slt), EX(sltu), EX(xor), EX(srl), EX(or), EX(and)
+  };
+  idex(pc, &table[decinfo.isa.instr.funct3]);
 }
-
-static OpcodeEntry op_imm_table32 [8] = {
-  EX(addw), EX(sllw), EMPTY, EMPTY, EMPTY, EX(srlw), EMPTY, EMPTY
-};
 
 static make_EHelper(op_imm32) {
-  idex(pc, &op_imm_table32[decinfo.isa.instr.funct3]);
+  static OpcodeEntry table [8] = {
+    EX(addw), EX(sllw), EMPTY, EMPTY, EMPTY, EX(srlw), EMPTY, EMPTY
+  };
+  idex(pc, &table[decinfo.isa.instr.funct3]);
 }
-
-static OpcodeEntry op_table [8] = {
-  EX(add), EX(sll), EX(slt), EX(sltu), EX(xor), EX(srl), EX(or), EX(and)
-};
-
-static OpcodeEntry op2_table [8] = {
-  EX(sub), EMPTY, EMPTY, EMPTY, EMPTY, EX(sra), EMPTY, EMPTY
-};
-
-static OpcodeEntry muldiv_table [8] = {
-  EX(mul), EX(mulh), EX(mulhsu), EX(mulhu), EX(div), EX(divu), EX(rem), EX(remu)
-};
 
 static make_EHelper(op) {
-  switch (decinfo.isa.instr.funct7) {
-    case 0:  idex(pc, &op_table[decinfo.isa.instr.funct3]); break;
-    case 1:  idex(pc, &muldiv_table[decinfo.isa.instr.funct3]); break;
-    case 32: idex(pc, &op2_table[decinfo.isa.instr.funct3]); break;
-    default: assert(0);
-  }
+  static OpcodeEntry table [3][8] = {
+    {EX(add), EX(sll), EX(slt), EX(sltu), EX(xor), EX(srl), EX(or), EX(and)},
+    {EX(mul), EX(mulh), EX(mulhsu), EX(mulhu), EX(div), EX(divu), EX(rem), EX(remu)},
+    {EX(sub), EMPTY, EMPTY, EMPTY, EMPTY, EX(sra), EMPTY, EMPTY},
+  };
+
+  uint32_t idx = decinfo.isa.instr.funct7;
+  if (idx == 32) idx = 2;
+  assert(idx <= 2);
+  idex(pc, &table[idx][decinfo.isa.instr.funct3]);
 }
-
-static OpcodeEntry op_table32 [8] = {
-  EX(addw), EX(sllw), EMPTY, EMPTY, EMPTY, EX(srlw), EMPTY, EMPTY
-};
-
-static OpcodeEntry op2_table32 [8] = {
-  EX(subw), EMPTY, EMPTY, EMPTY, EMPTY, EX(sraw), EMPTY, EMPTY
-};
-
-static OpcodeEntry muldiv_table32 [8] = {
-  EX(mulw), EMPTY, EMPTY, EMPTY, EX(divw), EX(divuw), EX(remw), EX(remuw)
-};
 
 static make_EHelper(op32) {
-  switch (decinfo.isa.instr.funct7) {
-    case 0:  idex(pc, &op_table32[decinfo.isa.instr.funct3]); break;
-    case 1:  idex(pc, &muldiv_table32[decinfo.isa.instr.funct3]); break;
-    case 32: idex(pc, &op2_table32[decinfo.isa.instr.funct3]); break;
-    default: printf("not implemented\n"); assert(0);
-  }
+  static OpcodeEntry table [3][8] = {
+    {EX(addw), EX(sllw), EMPTY, EMPTY, EMPTY, EX(srlw), EMPTY, EMPTY},
+    {EX(mulw), EMPTY, EMPTY, EMPTY, EX(divw), EX(divuw), EX(remw), EX(remuw)},
+    {EX(subw), EMPTY, EMPTY, EMPTY, EMPTY, EX(sraw), EMPTY, EMPTY},
+  };
+
+  uint32_t idx = decinfo.isa.instr.funct7;
+  if (idx == 32) idx = 2;
+  assert(idx <= 2);
+  idex(pc, &table[idx][decinfo.isa.instr.funct3]);
 }
 
-static OpcodeEntry system_table [8] = {
-  EX(priv), IDEX(csr, csrrw), IDEX(csr, csrrs), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
-};
-
 static make_EHelper(system) {
-  idex(pc, &system_table[decinfo.isa.instr.funct3]);
+  static OpcodeEntry table [8] = {
+    EX(priv), IDEX(csr, csrrw), IDEX(csr, csrrs), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY
+  };
+  idex(pc, &table[decinfo.isa.instr.funct3]);
 }
 
 static OpcodeEntry opcode_table [32] = {
@@ -94,16 +73,15 @@ static OpcodeEntry opcode_table [32] = {
 
 // RVC
 
-static OpcodeEntry C_10_100_table [8] = {
-  EMPTY, EMPTY, EX(jalr), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
-};
-
 static make_EHelper(C_10_100) {
+  static OpcodeEntry table [8] = {
+    EMPTY, EMPTY, EX(jalr), EMPTY, EMPTY, EMPTY, EMPTY, EMPTY,
+  };
   int cond_c_simm12_not0 = (decinfo.isa.instr.c_simm12 != 0);
   int cond_c_rd_rs1_not0 = (decinfo.isa.instr.c_rd_rs1 != 0);
   int cond_c_rs2_not0 = (decinfo.isa.instr.c_rs2 != 0);
   int idx = (cond_c_simm12_not0 << 2) | (cond_c_rd_rs1_not0 << 1) | cond_c_rs2_not0;
-  idex(pc, &C_10_100_table[idx]);
+  idex(pc, &table[idx]);
 }
 
 static OpcodeEntry rvc_table [3][8] = {
