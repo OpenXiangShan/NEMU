9cb1a3eee343fd13bd47b2e0ef5b6090452fdfe3
Zihao Yu <yuzihao@ict.ac.cn>
2019-12-11 22:01:45 +0800

    riscv64,difftest: add [m|s][status|cause|epc]

--- src/isa/riscv64/diff-test.c
+++ src/isa/riscv64/diff-test.c
@@ -1,5 +1,6 @@
 #include "nemu.h"
 #include "monitor/diff-test.h"
+#include "csr.h"
 
 bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
   if (memcmp(&cpu, ref_r, DIFFTEST_REG_SIZE)) {
@@ -16,4 +17,25 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 void isa_difftest_attach(void) {
   ref_difftest_memcpy_from_dut(0, guest_to_host(0), PMEM_SIZE);
   ref_difftest_setregs(&cpu);
+}
+
+void isa_difftest_getregs_hook(void) {
+  cpu.mstatus = mstatus->val;
+  cpu.mcause  = mcause->val;
+  cpu.mepc    = mepc->val;
+
+  rtlreg_t temp;
+  csr_read(&temp, 0x100); // sstatus
+  cpu.sstatus = temp;
+  cpu.scause  = scause->val;
+  cpu.sepc    = sepc->val;
+}
+
+void isa_difftest_setregs_hook(void) {
+  mstatus->val = cpu.mstatus;
+  mcause ->val = cpu.mcause ;
+  mepc   ->val = cpu.mepc   ;
+  //sstatus->val = cpu.sstatus;  // sstatus is a shadow of mstatus
+  scause ->val = cpu.scause ;
+  sepc   ->val = cpu.sepc   ;
 }
