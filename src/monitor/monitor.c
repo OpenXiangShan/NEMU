89243de088bca23019d11b7e6b610b3472d32775
Zihao Yu <yuzihao@ict.ac.cn>
2020-03-01 12:12:41 +0800

    monitor: handle mainargs in nexus-am

--- src/monitor/monitor.c
+++ src/monitor/monitor.c
@@ -4,8 +4,6 @@
 #include <getopt.h>
 #include <stdlib.h>
 
-#define MAINARGS_START (IMAGE_START + 0x1000)
-
 void init_log(const char *log_file);
 void init_isa();
 void init_regex();
@@ -13,7 +11,6 @@ void init_wp_pool();
 void init_device();
 void init_difftest(char *ref_so_file, long img_size, int port);
 
-static char *mainargs = NULL;
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
@@ -59,11 +56,6 @@ static inline long load_img() {
     assert(ret == 1);
 
     fclose(fp);
-
-    // mainargs
-    if (mainargs != NULL) {
-      strcpy(guest_to_host(MAINARGS_START), mainargs);
-    }
   }
   return size;
 }
@@ -73,16 +65,14 @@ static inline void parse_args(int argc, char *argv[]) {
     {"batch"    , no_argument      , NULL, 'b'},
     {"log"      , required_argument, NULL, 'l'},
     {"diff"     , required_argument, NULL, 'd'},
-    {"mainargs" , required_argument, NULL, 'm'},
     {"port"     , required_argument, NULL, 'p'},
     {"help"     , no_argument      , NULL, 'h'},
     {0          , 0                , NULL,  0 },
   };
   int o;
-  while ( (o = getopt_long(argc, argv, "-bhl:d:m:p:", table, NULL)) != -1) {
+  while ( (o = getopt_long(argc, argv, "-bhl:d:p:", table, NULL)) != -1) {
     switch (o) {
       case 'b': is_batch_mode = true; break;
-      case 'm': mainargs = optarg; break;
       case 'p': sscanf(optarg, "%d", &difftest_port); break;
       case 'l': log_file = optarg; break;
       case 'd': diff_so_file = optarg; break;
@@ -94,7 +84,6 @@ static inline void parse_args(int argc, char *argv[]) {
         printf("Usage: %s [OPTION...] IMAGE\n\n", argv[0]);
         printf("\t-b,--batch              run with batch mode\n");
         printf("\t-l,--log=FILE           output log to FILE\n");
-        printf("\t-m,--mainargs=ARGS      run guest with mainargs (AM only)\n");
         printf("\t-d,--diff=REF_SO        run DiffTest with reference REF_SO\n");
         printf("\t-p,--port=PORT          run DiffTest with port PORT\n");
         printf("\n");
