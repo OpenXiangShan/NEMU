4660a8af5d20582ce92e1887bd6d479d1754feb9
Zihao Yu <yuzihao@ict.ac.cn>
2020-05-18 09:59:14 +0800

    monitor,difftest,ref: let `dest` accept valid paddr in difftest_memcpy_from_dut()

--- src/isa/riscv64/difftest/dut.c
+++ src/isa/riscv64/difftest/dut.c
@@ -18,6 +18,6 @@ bool isa_difftest_checkregs(CPU_state *ref_r, vaddr_t pc) {
 }
 
 void isa_difftest_attach(void) {
-  ref_difftest_memcpy_from_dut(0, guest_to_host(0), PMEM_SIZE);
+  ref_difftest_memcpy_from_dut(PMEM_BASE, guest_to_host(0), PMEM_SIZE);
   ref_difftest_setregs(&cpu);
 }
