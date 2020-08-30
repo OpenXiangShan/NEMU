cdbbb15346fce3295c190f4aeed305a1a345863f
Zihao Yu <yuzihao@ict.ac.cn>
2020-08-30 17:51:57 +0800

    device,vga: support 800x600

--- src/device/vga.c
+++ src/device/vga.c
@@ -3,6 +3,16 @@
 #ifdef HAS_IOE
 
 //#define SHOW_SCREEN
+#define MODE_800x600
+
+#ifdef MODE_800x600
+# define SCREEN_W 800
+# define SCREEN_H 600
+#else
+# define SCREEN_W 400
+# define SCREEN_H 300
+#endif
+#define SCREEN_SIZE ((SCREEN_H * SCREEN_W) * sizeof(uint32_t))
 
 #include <device/map.h>
 #include <SDL2/SDL.h>
@@ -13,9 +23,6 @@
 #define SCREEN_MMIO 0xa1000100
 #define SYNC_PORT 0x104 // Note that this is not the standard
 #define SYNC_MMIO 0xa1000104
-#define SCREEN_H 300
-#define SCREEN_W 400
-#define SCREEN_SIZE ((SCREEN_H * SCREEN_W) * sizeof(uint32_t))
 
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
@@ -44,7 +51,11 @@ void init_vga() {
   char title[128];
   sprintf(title, "%s-NEMU", str(__ISA__));
   SDL_Init(SDL_INIT_VIDEO);
+#ifdef MODE_800x600
+  SDL_CreateWindowAndRenderer(SCREEN_W, SCREEN_H, 0, &window, &renderer);
+#else
   SDL_CreateWindowAndRenderer(SCREEN_W * 2, SCREEN_H * 2, 0, &window, &renderer);
+#endif
   SDL_SetWindowTitle(window, title);
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
@@ -55,7 +66,7 @@ void init_vga() {
   add_pio_map("screen", SCREEN_PORT, (void *)screensize_port_base, 8, vga_io_handler);
   add_mmio_map("screen", SCREEN_MMIO, (void *)screensize_port_base, 8, vga_io_handler);
 
-  vmem = (void *)new_space(0x80000);
+  vmem = (void *)new_space(SCREEN_SIZE);
   add_mmio_map("vmem", VMEM, (void *)vmem, SCREEN_SIZE, NULL);
 }
 #endif	/* HAS_IOE */
