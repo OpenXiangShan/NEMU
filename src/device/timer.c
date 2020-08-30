5b7be8a6804e6abee9339e98cf53c8b0e1c28c99
Zihao Yu <yuzihao@ict.ac.cn>
2020-08-30 17:35:10 +0800

    device,timer: provide us precision

--- src/device/timer.c
+++ src/device/timer.c
@@ -9,15 +9,12 @@
 static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
-  assert(offset == 0 || offset == 8 || offset == 12);
+  assert(offset == 0 || offset == 4);
   if (!is_write) {
     struct timeval now;
     gettimeofday(&now, NULL);
-    uint32_t seconds = now.tv_sec;
-    uint32_t useconds = now.tv_usec;
-    rtc_port_base[0] = seconds * 1000 + (useconds + 500) / 1000;
-    rtc_port_base[2] = useconds;
-    rtc_port_base[3] = seconds;
+    rtc_port_base[0] = now.tv_usec;
+    rtc_port_base[1] = now.tv_sec;
   }
 }
 
@@ -29,8 +26,8 @@ static void timer_intr() {
 }
 
 void init_timer() {
-  rtc_port_base = (void*)new_space(16);
-  add_pio_map("rtc", RTC_PORT, (void *)rtc_port_base, 16, rtc_io_handler);
-  add_mmio_map("rtc", RTC_MMIO, (void *)rtc_port_base, 16, rtc_io_handler);
+  rtc_port_base = (void*)new_space(8);
+  add_pio_map("rtc", RTC_PORT, (void *)rtc_port_base, 8, rtc_io_handler);
+  add_mmio_map("rtc", RTC_MMIO, (void *)rtc_port_base, 8, rtc_io_handler);
   add_alarm_handle(timer_intr);
 }
