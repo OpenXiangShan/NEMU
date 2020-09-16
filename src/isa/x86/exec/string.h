4a1a6dfac9c8f3d3b4a576018ee27ebb2a55f428
Zihao Yu <yuzihao@ict.ac.cn>
2020-09-16 11:15:24 +0800

    x86,exec,string: update eflags for scas

--- src/isa/x86/exec/string.h
+++ src/isa/x86/exec/string.h
@@ -54,13 +54,21 @@ static inline def_EHelper(scas) {
   word_t count = (s->isa.rep_flags ? cpu.ecx : 1);
   if (count != 0) {
     rtl_lm(s, s0, &cpu.edi, 0, id_dest->width);
-    rtl_setrelop(s, RELOP_EQ, s0, s0, dsrc1);
-    rtl_set_ZF(s, s0);
+    rtl_setrelop(s, RELOP_EQ, s1, s0, dsrc1);
+    rtl_set_ZF(s, s1);
     rtl_addi(s, &cpu.edi, &cpu.edi, id_dest->width);
   }
   if (s->isa.rep_flags && count != 0) {
     cpu.ecx --;
     if ((count - 1 != 0) && (is_repnz ^ cpu.ZF)) rtl_j(s, cpu.pc);
+  } else {
+    rtl_sub(s, s1, dsrc1, s0);
+    rtl_update_ZFSF(s, s1, id_dest->width);
+    rtl_is_sub_carry(s, s1, dsrc1, s0);
+    rtl_set_CF(s, s1);
+    rtl_sub(s, s1, dsrc1, s0);
+    rtl_is_sub_overflow(s, s1, s1, dsrc1, s0, id_dest->width);
+    rtl_set_OF(s, s1);
   }
 
   print_asm("stos %%eax, (%%edi)");
