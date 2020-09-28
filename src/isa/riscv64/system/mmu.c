4ec7928d6660a1a5ebe6e7719895433b334197a0
William Wang <zeweiwang@outlook.com>
2020-09-28 15:57:27 +0800

    Difftest: fix PF encoding in force_raise_pf

--- src/isa/riscv64/system/mmu.c
+++ src/isa/riscv64/system/mmu.c
@@ -187,24 +187,19 @@ int force_raise_pf(vaddr_t vaddr, int type){
   bool is_ifetch = (type == MEM_TYPE_IFETCH);
 
   if(cpu.need_disambiguate){
-    if(is_ifetch && cpu.disambiguation_state.exceptionNo == EX_IAF){
+    if(is_ifetch && cpu.disambiguation_state.exceptionNo == EX_IPF){
       stval->val = vaddr;
-      cpu.mem_exception = EX_IAF;
+      cpu.mem_exception = EX_IPF;
       return MEM_RET_FAIL;
-    } else if(!cpu.amo && type == MEM_TYPE_READ && cpu.disambiguation_state.exceptionNo == EX_LAF){
+    } else if(!is_ifetch && type == MEM_TYPE_READ && cpu.disambiguation_state.exceptionNo == EX_LPF){
       if (cpu.mode == MODE_M) mtval->val = vaddr;
       else stval->val = vaddr;
-      cpu.mem_exception = EX_LAF;
+      cpu.mem_exception = EX_LPF;
       return MEM_RET_FAIL;
-    } else if(type == MEM_TYPE_WRITE && cpu.disambiguation_state.exceptionNo == EX_SAF){
+    } else if(type == MEM_TYPE_WRITE && cpu.disambiguation_state.exceptionNo == EX_SPF){
       if (cpu.mode == MODE_M) mtval->val = vaddr;
       else stval->val = vaddr;
-      cpu.mem_exception = EX_SAF;
-      return MEM_RET_FAIL;
-    } else if(cpu.amo && type == MEM_TYPE_READ && cpu.disambiguation_state.exceptionNo == EX_LAF){
-      if (cpu.mode == MODE_M) mtval->val = vaddr;
-      else stval->val = vaddr;
-      cpu.mem_exception = EX_SAF;
+      cpu.mem_exception = EX_SPF;
       return MEM_RET_FAIL;
     }
   }
