92be042442bb63975cd2255a22482fd438e9e247
Zihao Yu <yuzihao@ict.ac.cn>
2020-11-14 17:47:26 +0800

    device,vga: fix compile error when SHOW_SCREEN is not defined

--- src/device/vga.c
+++ src/device/vga.c
@@ -26,8 +26,10 @@
 #define VGACTL_PORT 0x100 // Note that this is not the standard
 #define VGACTL_MMIO 0xa1000100
 
+#ifdef SHOW_SCREEN
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
+#endif
 
 static uint32_t (*vmem) [SCREEN_W] = NULL;
 static uint32_t *vgactl_port_base = NULL;
