770168aad27f5d2a78f5929e40d7fa19de4c62c8
Zihao Yu <yuzihao@ict.ac.cn>
2020-11-17 15:20:50 +0800

    x86,exec,arith: adjust the update of eflags for mul/imul

--- src/isa/x86/exec/arith.h
+++ src/isa/x86/exec/arith.h
@@ -202,22 +202,47 @@ static inline def_EHelper(sbb) {
 }
 
 static inline def_EHelper(mul) {
-  rtl_lr(s, s0, R_EAX, id_dest->width);
-  rtl_mul_lo(s, s1, ddest, s0);
-
   switch (id_dest->width) {
     case 1:
+      rtl_lr(s, s0, R_EAX, 1);
+      rtl_mul_lo(s, s1, ddest, s0);
+#ifndef __PA__
+      rtl_update_ZFSF(s, s1, id_dest->width);
+      rtl_andi(s, s0, s1, 0xff00);
+      rtl_setrelopi(s, RELOP_NE, s0, s0, 0);
+      rtl_set_OF(s, s0);
+      rtl_set_CF(s, s0);
+#endif
       rtl_sr(s, R_AX, s1, 2);
       break;
     case 2:
+      rtl_lr(s, s0, R_EAX, 2);
+      rtl_mul_lo(s, s1, ddest, s0);
+#ifndef __PA__
+      rtl_update_ZFSF(s, s1, id_dest->width);
+      rtl_shri(s, s0, s1, 16);
+      rtl_setrelopi(s, RELOP_NE, s0, s0, 0);
+      rtl_set_OF(s, s0);
+      rtl_set_CF(s, s0);
+#endif
       rtl_sr(s, R_AX, s1, 2);
       rtl_shri(s, s1, s1, 16);
       rtl_sr(s, R_DX, s1, 2);
       break;
     case 4:
-      rtl_mul_hi(s, s0, ddest, s0);
-      rtl_sr(s, R_EDX, s0, 4);
-      rtl_sr(s, R_EAX, s1, 4);
+      ; rtlreg_t *pdest = ddest;
+      if (ddest == &cpu.edx) {
+        rtl_mv(s, s0, ddest);
+        pdest = s0;
+      }
+      rtl_mul_hi(s, &cpu.edx, pdest, &cpu.eax);
+      rtl_mul_lo(s, &cpu.eax, pdest, &cpu.eax);
+#ifndef __PA__
+      rtl_update_ZFSF(s, &cpu.eax, id_dest->width);
+      rtl_setrelopi(s, RELOP_NE, s0, &cpu.edx, 0);
+      rtl_set_OF(s, s0);
+      rtl_set_CF(s, s0);
+#endif
       break;
     default: assert(0);
   }
@@ -230,17 +255,25 @@ static inline def_EHelper(imul1) {
   switch (id_dest->width) {
     case 1:
       rtl_lr(s, s0, R_EAX, 1);
+      rtl_sext(s, s0, s0, 1);
+      rtl_sext(s, ddest, ddest, 1);
       rtl_imul_lo(s, s1, ddest, s0);
 #ifndef __PA__
-      rtl_update_ZFSF(s, s1, id_dest->width);
+      rtl_update_ZFSF(s, s1, 1);
+      rtl_sext(s, s0, s1, 1);
+      rtl_setrelop(s, RELOP_NE, s0, s0, s1);
 #endif
       rtl_sr(s, R_AX, s1, 2);
       break;
     case 2:
       rtl_lr(s, s0, R_EAX, 2);
+      rtl_sext(s, s0, s0, 2);
+      rtl_sext(s, ddest, ddest, 2);
       rtl_imul_lo(s, s1, ddest, s0);
 #ifndef __PA__
-      rtl_update_ZFSF(s, s1, id_dest->width);
+      rtl_update_ZFSF(s, s1, 2);
+      rtl_sext(s, s0, s1, 2);
+      rtl_setrelop(s, RELOP_NE, s0, s0, s1);
 #endif
       rtl_sr(s, R_AX, s1, 2);
       rtl_shri(s, s1, s1, 16);
@@ -255,12 +288,17 @@ static inline def_EHelper(imul1) {
       rtl_imul_hi(s, &cpu.edx, pdest, &cpu.eax);
       rtl_imul_lo(s, &cpu.eax, pdest, &cpu.eax);
 #ifndef __PA__
-      rtl_update_ZFSF(s, &cpu.eax, id_dest->width);
+      rtl_update_ZFSF(s, &cpu.eax, 4);
+      rtl_msb(s, s0, &cpu.eax, 4);
+      rtl_add(s, s0, &cpu.edx, s0);
+      rtl_setrelopi(s, RELOP_NE, s0, s0, 0);
 #endif
       break;
     default: assert(0);
   }
 
+  rtl_set_CF(s, s0);
+  rtl_set_OF(s, s0);
   print_asm_template1(imul);
 }
 
@@ -299,11 +337,29 @@ static inline def_EHelper(imul2) {
 
 // imul with three operands
 static inline def_EHelper(imul3) {
-  rtl_sext(s, dsrc1, dsrc1, id_dest->width);
   rtl_sext(s, dsrc2, dsrc2, id_dest->width);
 
+#if !defined(__PA__) && defined(DIFF_TEST)
+  if (id_dest->width == 4) {
+    rtl_imul_hi(s, s1, dsrc2, dsrc1);
+  }
+#endif
+
   rtl_imul_lo(s, ddest, dsrc2, dsrc1);
-#ifndef __PA__
+
+#if !defined(__PA__) && defined(DIFF_TEST)
+  if (id_dest->width == 2) {
+    rtl_sext(s, s0, ddest, id_dest->width);
+    rtl_setrelop(s, RELOP_NE, s0, s0, ddest);
+  } else if (id_dest->width == 4) {
+    rtl_msb(s, s0, ddest, id_dest->width);
+    rtl_add(s, s0, s1, s0);
+    rtl_setrelopi(s, RELOP_NE, s0, s0, 0);
+  } else {
+    assert(0);
+  }
+  rtl_set_CF(s, s0);
+  rtl_set_OF(s, s0);
   rtl_update_ZFSF(s, ddest, id_dest->width);
 #endif
   operand_write(s, id_dest, ddest);
