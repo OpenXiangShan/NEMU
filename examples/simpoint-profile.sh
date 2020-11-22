2529c3e7d1492539d3400117fd70236689f49a61
Zhou Yaoyang <shinezyy@qq.com>
2020-11-22 15:21:51 +0800

    Example scripts to do simpoint profiling and checkpointing

---
+++ examples/simpoint-profile.sh
@@ -0,0 +1,1 @@
+./build/riscv64-nemu-interpreter $AM_HOME/apps/microbench/build/microbench-riscv64-noop.bin --simpoint-profile -D outputs -C simpoint_profile -w microbench
---
+++ examples/take-simpoint-checkpoint.sh
@@ -0,0 +1,2 @@
+./build/riscv64-nemu-interpreter $AM_HOME/apps/microbench/build/microbench-riscv64-noop.bin -D outputs -w microbench -C take_cpt -S outputs/simpoint_profile
+
