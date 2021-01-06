1a88dd6e7459e123cc06e697ee11cef9a6c61187
Zhou Yaoyang <shinezyy@qq.com>
2021-01-06 17:40:04 +0800

    [Batch] Allow to pass codename (tag) from cmdline to repeat the same task

--- nemu_parallel.py
+++ nemu_parallel.py
@@ -12,12 +12,15 @@ from multiprocessing import Pool
 # -D outputs -w xz_cpu2006 -C simpoint_profile --simpoint-profile --interval 100000000 -b
 
 class BatchTask:
-    def __init__(self, ver, taskname):
+    def __init__(self, ver, taskname, codename):
         self.ver = ver
         self.source_data_dir = 'outputs3'
         self.top_out_dir = '/home51/zyy/expri_results'
         self.task = taskname
-        self.task_name = taskname + f'_{self.ver}'
+        if len(codename):
+            self.task_name = taskname + f'_{self.ver}_{codename}'
+        else:
+            self.task_name = taskname + f'_{self.ver}'
 
         self.simpoint_profile_dir = None
         if taskname == 'take_simpoint_checkpoint':
@@ -37,6 +40,11 @@ class BatchTask:
             '--checkpoint-interval', 8 * 10**9, # Billion
             ],
 
+        'normal_checkpoint': [
+            '-D', self.top_out_dir,
+            '--checkpoint-interval', 500 * 10**6, # Million
+            ],
+
         'take_simpoint_checkpoint': [
             '--interval', '50000000',
             '-D', self.top_out_dir,
@@ -50,6 +58,9 @@ class BatchTask:
             return True
 
         if self.task == 'betapoint_profile':
+            return True
+
+        if self.task == 'normal_checkpoint':
             return True
 
         if self.task == 'take_simpoint_checkpoint':
@@ -111,6 +122,8 @@ class BatchTask:
 
 def main():
     parser = argparse.ArgumentParser()
+    parser.add_argument('-c', '--codename', help='codename',
+            type=str, action='store', required=False, default='')
     parser.add_argument('-v', '--spec-version', help='`06` or `17`',
             type=str, action='store', required=True)
     parser.add_argument('-w', '--workload', help='a specific workload',
@@ -120,13 +133,14 @@ def main():
             choices=[
                 'simpoint_profile',
                 'betapoint_profile',
+                'normal_checkpoint',
                 'take_simpoint_checkpoint',
                 ])
 
     args = parser.parse_args()
 
     ver = args.spec_version
-    bt = BatchTask(ver, args.task)
+    bt = BatchTask(ver, args.task, args.codename)
 
     bbl_dir = f'bbl_kernel_gen/spec{ver}_bbl'
     tuples = []
