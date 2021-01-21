e154751c3d47eeafb65268ba08d7ccfa8e43f345
Zihao Yu <yuzihao@ict.ac.cn>
2021-01-21 13:10:54 +0800

    user,syscall: add dummy sigaction() and stat64()

--- src/user/syscall.c
+++ src/user/syscall.c
@@ -12,28 +12,7 @@ static inline int user_fd(int fd) {
   return fd;
 }
 
-static inline void user_sys_exit(int status) {
-  void set_nemu_state(int state, vaddr_t pc, int halt_ret);
-  set_nemu_state(NEMU_END, cpu.pc, status);
-}
-
-static inline word_t user_sys_brk(word_t new_brk) {
-  if (new_brk == 0) return user_state.brk;
-  if (new_brk >= user_state.brk_page) {
-    uint32_t size = ROUNDUP(new_brk - user_state.brk_page + 1, PAGE_SIZE);
-    user_mmap(user_to_host(user_state.brk_page), size, PROT_READ | PROT_WRITE,
-      MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS, -1, 0);
-    user_state.brk_page += size;
-  }
-  user_state.brk = new_brk;
-  return new_brk;
-}
-
-static inline word_t user_sys_fstat64(int fd, word_t statbuf) {
-  struct stat buf;
-  int ret = fstat(fd, &buf);
-  if (ret != 0) return ret;
-
+static inline void translate_stat(struct stat *hostbuf, word_t user) {
   struct target_stat64 {
     unsigned short st_dev;
     unsigned char __pad0[10];
@@ -65,25 +44,59 @@ static inline word_t user_sys_fstat64(int fd, word_t statbuf) {
     uint32_t target_st_ctime_nsec;
 
     unsigned long long st_ino;
-  } __attribute__((packed)) *userbuf = user_to_host(statbuf);
+  } __attribute__((packed)) *userbuf = user_to_host(user);
 
-  userbuf->st_dev = buf.st_dev;
-  userbuf->__st_ino = buf.st_ino;
-  userbuf->st_mode = buf.st_mode;
-  userbuf->st_nlink = buf.st_nlink;
-  userbuf->st_uid = buf.st_uid;
-  userbuf->st_gid = buf.st_gid;
-  userbuf->st_rdev = buf.st_rdev;
-  userbuf->st_size = buf.st_size;
-  userbuf->st_blksize = buf.st_blksize;
-  userbuf->st_blocks = buf.st_blocks;
-  userbuf->target_st_atime = buf.st_atime;
-  userbuf->target_st_atime_nsec = buf.st_atim.tv_nsec;
-  userbuf->target_st_mtime = buf.st_mtime;
-  userbuf->target_st_mtime_nsec = buf.st_mtim.tv_nsec;
-  userbuf->target_st_ctime = buf.st_ctime;
-  userbuf->target_st_ctime_nsec = buf.st_ctim.tv_nsec;
-  userbuf->st_ino = buf.st_ino;
+  userbuf->st_dev = hostbuf->st_dev;
+  userbuf->__st_ino = hostbuf->st_ino;
+  userbuf->st_mode = hostbuf->st_mode;
+  userbuf->st_nlink = hostbuf->st_nlink;
+  userbuf->st_uid = hostbuf->st_uid;
+  userbuf->st_gid = hostbuf->st_gid;
+  userbuf->st_rdev = hostbuf->st_rdev;
+  userbuf->st_size = hostbuf->st_size;
+  userbuf->st_blksize = hostbuf->st_blksize;
+  userbuf->st_blocks = hostbuf->st_blocks;
+  userbuf->target_st_atime = hostbuf->st_atime;
+  userbuf->target_st_atime_nsec = hostbuf->st_atim.tv_nsec;
+  userbuf->target_st_mtime = hostbuf->st_mtime;
+  userbuf->target_st_mtime_nsec = hostbuf->st_mtim.tv_nsec;
+  userbuf->target_st_ctime = hostbuf->st_ctime;
+  userbuf->target_st_ctime_nsec = hostbuf->st_ctim.tv_nsec;
+  userbuf->st_ino = hostbuf->st_ino;
+}
+
+static inline void user_sys_exit(int status) {
+  void set_nemu_state(int state, vaddr_t pc, int halt_ret);
+  set_nemu_state(NEMU_END, cpu.pc, status);
+}
+
+static inline word_t user_sys_brk(word_t new_brk) {
+  if (new_brk == 0) return user_state.brk;
+  if (new_brk >= user_state.brk_page) {
+    uint32_t size = ROUNDUP(new_brk - user_state.brk_page + 1, PAGE_SIZE);
+    user_mmap(user_to_host(user_state.brk_page), size, PROT_READ | PROT_WRITE,
+      MAP_PRIVATE | MAP_FIXED | MAP_ANONYMOUS, -1, 0);
+    user_state.brk_page += size;
+  }
+  user_state.brk = new_brk;
+  return new_brk;
+}
+
+static inline word_t user_sys_stat64(const char *pathname, word_t statbuf) {
+  struct stat buf;
+  int ret = stat(pathname, &buf);
+  if (ret == 0) {
+    translate_stat(&buf, statbuf);
+  }
+  return ret;
+}
+
+static inline word_t user_sys_fstat64(int fd, word_t statbuf) {
+  struct stat buf;
+  int ret = fstat(fd, &buf);
+  if (ret == 0) {
+    translate_stat(&buf, statbuf);
+  }
   return ret;
 }
 
@@ -130,8 +143,10 @@ uintptr_t host_syscall(uintptr_t id, uintptr_t arg1, uintptr_t arg2, uintptr_t a
     case 85: ret = readlink((void *)arg1, (void *)arg2, arg3); break;
     case 91: ret = user_munmap((void *)arg1, arg2); break;
     case 122: ret = uname((void *)arg1); break;
+    case 174: ret = 0; break; // sigaction
     case 192: ret = (uintptr_t)user_mmap((void *)arg1, arg2,
                   arg3, arg4, user_fd(arg5), arg6 << 12); break;
+    case 195: ret = user_sys_stat64((void *)arg1, arg2); break;
     case 197: ret = user_sys_fstat64(user_fd(arg1), arg2); break;
     case 243: ret = user_sys_set_thread_area(arg1); break;
     case 295: ret = openat(user_fd(arg1), (void *)arg2, arg3, arg4); break;
