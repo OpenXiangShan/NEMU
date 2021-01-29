7aeda7496fa1197342a8da19e3576a34142ce59b
Zihao Yu <yuzihao@ict.ac.cn>
2021-01-29 17:57:09 +0800

    utils,statistic: use gettimeofday()
    * it reports more precise result than clock()

--- src/device/timer.c
+++ src/device/timer.c
@@ -10,7 +10,6 @@ static uint32_t *rtc_port_base = NULL;
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
   if (!is_write && offset == 4) {
-    uint64_t get_time();
     uint64_t us = get_time();
     rtc_port_base[0] = (uint32_t)us;
     rtc_port_base[1] = us >> 32;
--- src/utils/statistic.c
+++ src/utils/statistic.c
@@ -1,17 +1,28 @@
 #include <common.h>
-#include <time.h>
 #include <locale.h>
 
+#define USE_GETTIMEOFDAY
+#ifdef USE_GETTIMEOFDAY
+#include <sys/time.h>
+#else
+#include <time.h>
 static_assert(CLOCKS_PER_SEC == 1000000, "CLOCKS_PER_SEC != 1000000");
 static_assert(sizeof(clock_t) == 8, "sizeof(clock_t) != 8");
+#endif
 
 uint64_t g_timer = 0; // unit: us
 uint64_t g_nr_guest_instr = 0;
 
 uint64_t get_time() {
+#ifdef USE_GETTIMEOFDAY
+  struct timeval now;
+  gettimeofday(&now, NULL);
+  uint64_t us = now.tv_sec * 1000000 + now.tv_usec;
+#else
   struct timespec now;
   clock_gettime(CLOCK_MONOTONIC_COARSE, &now);
   uint64_t us = now.tv_sec * 1000000 + now.tv_nsec / 1000;
+#endif
   return us;
 }
 
