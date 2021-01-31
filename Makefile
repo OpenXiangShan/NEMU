OpenXiangShan/NEMU

commit 828a0fbcdee68b82a14b00555055c0a48ee60b63

Makefile: remove wrong include

Makefile
@@ -74,7 +74,6 @@ $(DIFF_REF_SO):
 
 endif
 
-include $(NEMU_HOME)/scripts/Makefile.git
 compile_git:
 	$(call git_commit, "compile")
 $(BINARY): compile_git

commit 84464101e5f90e5ebd15f3608688f52a311721a2

Makefile: fix error introduced by commit 828a0fb

Makefile
@@ -36,9 +36,10 @@ $(SOFTFLOAT):
 .PHONY: $(SOFTFLOAT)
 endif
 
-include $(NEMU_HOME)/scripts/isa.mk
 include $(NEMU_HOME)/scripts/git.mk
 include $(NEMU_HOME)/scripts/config.mk
+include $(NEMU_HOME)/scripts/isa.mk
+include $(NEMU_HOME)/scripts/Makefile
 
 ifndef SHARE
 DIFF ?= qemu-dl