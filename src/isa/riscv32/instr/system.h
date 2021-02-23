2d06c1033dba4cd3fe5c1c94c576f4b409caa71a
Zihao Yu <yuzihao@ict.ac.cn>
2021-02-23 22:13:13 +0800

    riscv32,instr,system: small refactor

--- src/isa/riscv32/instr/system.h
+++ src/isa/riscv32/instr/system.h
@@ -1,11 +1,15 @@
 #ifndef __ICS_EXPORT
+
+#define csr_difftest()  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 3))
+#define priv_difftest() IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2))
+
 def_EHelper(csrrw) {
-  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 3));
+  csr_difftest();
   rtl_hostcall(s, HOSTCALL_CSR, ddest, dsrc1, id_src2->imm);
 }
 
 def_EHelper(csrrs) {
-  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 3));
+  csr_difftest();
   rtl_hostcall(s, HOSTCALL_CSR, s0, NULL, id_src2->imm);
   rtl_or(s, s1, s0, dsrc1);
   rtl_mv(s, ddest, s0);
@@ -13,18 +17,18 @@ def_EHelper(csrrs) {
 }
 
 def_EHelper(ecall) {
-  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+  priv_difftest();
   rtl_trap(s, s->pc, 9);
 }
 
 def_EHelper(sret) {
-  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+  priv_difftest();
   rtl_hostcall(s, HOSTCALL_PRIV, s0, NULL, 0x102);
   rtl_jr(s, s0);
 }
 
 def_EHelper(sfence_vma) {
-  IFUNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+  priv_difftest();
   rtl_hostcall(s, HOSTCALL_PRIV, NULL, NULL, 0x120);
 }
 #endif
