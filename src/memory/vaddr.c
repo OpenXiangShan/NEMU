5fa982cb70fd949c041a99d984466d3bacaed8ba
Yinan Xu <xuyinan1997@gmail.com>
2021-03-03 11:00:21 +0800

    vaddr: print rdata in XiangShan debug mode

--- src/memory/vaddr.c
+++ src/memory/vaddr.c
@@ -42,10 +42,12 @@ static word_t vaddr_mmu_read(struct Decode *s, vaddr_t addr, int len, int type) 
   int ret = pg_base & PAGE_MASK;
   if (ret == MEM_RET_OK) {
     addr = pg_base | (addr & PAGE_MASK);
+    word_t rdata = paddr_read(addr, len);
 #ifdef XIANGSHAN_DEBUG
-    printf("[NEMU] mmu_read: vaddr 0x%lx, paddr 0x%lx\n", vaddr, addr);
+    printf("[NEMU] mmu_read: vaddr 0x%lx, paddr 0x%lx, rdata 0x%lx\n",
+      vaddr, addr, rdata);
 #endif
-    return paddr_read(addr, len);
+    return rdata;
   } else if (len != 1 && ret == MEM_RET_CROSS_PAGE) {
     return vaddr_read_cross_page(addr, len, type);
   }
@@ -59,7 +61,7 @@ static void vaddr_mmu_write(struct Decode *s, vaddr_t addr, int len, word_t data
   if (ret == MEM_RET_OK) {
     addr = pg_base | (addr & PAGE_MASK);
 #ifdef XIANGSHAN_DEBUG
-    printf("[NEMU] mmu_write: vaddr 0x%lx, paddr 0x%lx, len = %d, data = 0x%lx",
+    printf("[NEMU] mmu_write: vaddr 0x%lx, paddr 0x%lx, len %d, data 0x%lx\n",
       vaddr, addr, len, data);
 #endif
     paddr_write(addr, len, data);
