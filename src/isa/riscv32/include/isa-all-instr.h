07ac5a50e7422a677c6c5f60af99e5df23d7e279
Zihao Yu <yuzihao@ict.ac.cn>
2021-03-07 16:13:04 +0800

    riscv32,instr: add standard pseudo instructions

--- src/isa/riscv32/include/isa-all-instr.h
+++ src/isa/riscv32/include/isa-all-instr.h
@@ -3,15 +3,18 @@
 
 #define INSTR_NULLARY(f) \
   f(inv) f(nemu_trap) f(ecall) f(sret) f(sfence_vma) f(ret)
-#define INSTR_UNARY(f)
+#define INSTR_UNARY(f) \
+  f(li_0) f(li_1)
 #define INSTR_BINARY(f) \
-  f(lui) f(auipc) f(jal) f(j) f(jr) \
-  f(lw) f(sw) f(lh) f(lb) f(lhu) f(lbu) f(sh) f(sb)
+  f(lui) f(auipc) f(jal) \
+  f(lw) f(sw) f(lh) f(lb) f(lhu) f(lbu) f(sh) f(sb) \
+  f(mv) f(j) f(jal_ra) f(jr)
 #define INSTR_TERNARY(f) \
   f(add) f(sll) f(srl) f(slt) f(sltu) f(xor) f(or) f(sub) f(sra) f(and) \
   f(addi) f(slli) f(srli) f(slti) f(sltui) f(xori) f(ori) f(srai) f(andi) \
   f(jalr) f(jr_imm) f(beq) f(bne) f(blt) f(bge) f(bltu) f(bgeu) \
   f(mul) f(mulh) f(mulhu) f(mulhsu) f(div) f(divu) f(rem) f(remu) \
-  f(csrrw) f(csrrs)
+  f(csrrw) f(csrrs) \
+  f(li) f(beqz) f(bnez) f(blez) f(bgez) f(bltz) f(bgtz)
 
 def_all_EXEC_ID();
