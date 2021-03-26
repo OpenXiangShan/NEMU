1c44c639fa34e0846151a146585f980c2990cc41
Zihao Yu <yuzihao@ict.ac.cn>
2021-03-26 09:13:14 +0800

    cpu,tcache: use temp pool instead of malloc to fix memory leak

--- src/cpu/tcache.c
+++ src/cpu/tcache.c
@@ -1,11 +1,24 @@
 #include <cpu/decode.h>
 #include <cpu/cpu.h>
-#include <stdlib.h>
 
-#define TCACHE_SIZE (1024 * 8)
+#define TCACHE_SIZE (8 * 1024)
+#define TCACHE_TMP_SIZE (TCACHE_SIZE / 4 + 2)
+#define BB_POOL_SIZE 1024
+#define BB_LIST_SIZE 1024
+
+typedef struct bb_t {
+  Decode *s;
+  struct bb_t *next;
+  vaddr_t pc;
+} bb_t;
 
 static Decode tcache_pool[TCACHE_SIZE] = {};
 static int tc_idx = 0;
+static Decode tcache_tmp_pool[TCACHE_TMP_SIZE] = {};
+static bool tcache_tmp_bitmap[TCACHE_TMP_SIZE] = {};
+static bb_t bb_pool[BB_POOL_SIZE] = {};
+static int bb_idx = 0;
+static bb_t bb_list [BB_LIST_SIZE] = {};
 static const void **g_special_exec_table = NULL;
 
 static const void* get_nemu_decode() {
@@ -26,21 +39,24 @@ static inline Decode* tcache_new(vaddr_t pc) {
   return &tcache_pool[tc_idx ++];
 }
 
-static inline Decode* tcache_new_malloc(vaddr_t pc) {
-  return tcache_entry_init(malloc(sizeof(Decode)), pc);
+static inline Decode* tcache_tmp_new(vaddr_t pc) {
+  int i;
+  for (i = 0; i < TCACHE_TMP_SIZE; i ++) {
+    if (tcache_tmp_bitmap[i] == 0) {
+      tcache_tmp_bitmap[i] = 1;
+      return tcache_entry_init(&tcache_tmp_pool[i], pc);
+    }
+  }
+  assert(0);
+  return NULL;
 }
 
+static inline void tcache_tmp_free(Decode *s) {
+  int idx = s - tcache_tmp_pool;
+  assert(idx >= 0 && idx < TCACHE_TMP_SIZE);
+  tcache_tmp_bitmap[idx] = 0;
+}
 
-#define BB_POOL_SIZE 512
-
-typedef struct bb_t {
-  Decode *s;
-  struct bb_t *next;
-  vaddr_t pc;
-} bb_t;
-
-static bb_t bb_pool[BB_POOL_SIZE] = {};
-static int bb_idx = 0;
 
 static inline bb_t* bb_new(Decode *s, vaddr_t pc, bb_t *next) {
   if (bb_idx == BB_POOL_SIZE) return NULL;
@@ -51,9 +67,6 @@ static inline bb_t* bb_new(Decode *s, vaddr_t pc, bb_t *next) {
   bb->next = next;
   return bb;
 }
-
-#define BB_LIST_SIZE 512
-static bb_t bb_list [BB_LIST_SIZE] = {};
 
 static inline bb_t* bb_hash(vaddr_t pc) {
   int idx = (pc / CONFIG_ILEN_MIN) % BB_LIST_SIZE;
@@ -101,7 +114,7 @@ static void tcache_bb_fetch(Decode *this, int is_taken, vaddr_t jpc) {
     if (is_taken) { this->tnext = bb->s; }
     else { this->ntnext = bb->s; }
   } else {
-    Decode *ret = tcache_new_malloc(jpc);
+    Decode *ret = tcache_tmp_new(jpc);
     if (is_taken) { ret->tnext = this; this->tnext = ret; }
     else { ret->ntnext = this; this->ntnext = ret; }
   }
@@ -111,6 +124,7 @@ static void tcache_flush() {
   tc_idx = 0;
   bb_idx = 0;
   memset(bb_list, -1, sizeof(bb_list));
+  memset(tcache_tmp_bitmap, 0, sizeof(tcache_tmp_bitmap));
 }
 
 enum { TCACHE_BB_BUILDING, TCACHE_RUNNING };
@@ -137,7 +151,7 @@ Decode* tcache_decode(Decode *s, const void **exec_table) {
       s = bb->s;
       if (old->tnext)  { old->tnext->tnext = s; }
       if (old->ntnext) { old->ntnext->ntnext = s; }
-      free(old);
+      tcache_tmp_free(old);
       return s;
     }
   }
@@ -163,7 +177,7 @@ full: old->EHelper = get_nemu_decode(); // decode again
     tcache_state = TCACHE_BB_BUILDING;
     if (old->tnext)  { old->tnext->tnext = s; }
     if (old->ntnext) { old->ntnext->ntnext = s; }
-    free(old);
+    tcache_tmp_free(old);
   }
 
   s->idx_in_bb = idx_in_bb ++;
@@ -175,7 +189,7 @@ full: old->EHelper = get_nemu_decode(); // decode again
       tcache_flush();
       idx_in_bb = 1;
       // decode this instruction again
-      Decode *again = tcache_new_malloc(thispc);
+      Decode *again = tcache_tmp_new(thispc);
       save_globals(again);
       tcache_state = TCACHE_RUNNING;
       longjmp_exec(NEMU_EXEC_AGAIN);
@@ -220,5 +234,5 @@ Decode* tcache_handle_flush(vaddr_t snpc) {
 Decode* tcache_init(const void **special_exec_table, vaddr_t reset_vector) {
   tcache_flush();
   g_special_exec_table = special_exec_table;
-  return tcache_new_malloc(reset_vector);
+  return tcache_tmp_new(reset_vector);
 }
