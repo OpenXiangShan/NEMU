OpenXiangShan/NEMU

commit c3285ed6a5f481fe352f20fff0c51f44f1a25d5a

C++: remove C-specific code to compile with g++
- Replace designated initializers with init functions
- Rename or/xor/and to nemuor/nemuxor/nemuand
- Replace (void *) with (type *), and explicitly convert types
- RV64 System/User can be compiled; RV64 System has been tested.

src/isa/riscv64/system/mmu.c
@@ -79,8 +79,9 @@ static paddr_t ptw(vaddr_t vaddr, int type) {
   word_t p_pte; // pte pointer
   PTE pte;
   int level;
-  int64_t vaddr39 = vaddr << (64 - 39);
+  uint64_t vaddr39 = vaddr << (64 - 39);
   vaddr39 >>= (64 - 39);
+  bool is_write = (type == MEM_TYPE_WRITE);
   if (vaddr39 != vaddr) goto bad;
   for (level = PTW_LEVEL - 1; level >= 0;) {
     p_pte = pg_base + VPNi(vaddr, level) * PTE_SIZE;
@@ -106,7 +107,6 @@ static paddr_t ptw(vaddr_t vaddr, int type) {
     pg_base = (pg_base & ~pg_mask) | (vaddr & pg_mask & ~PGMASK);
   }
 
-  bool is_write = (type == MEM_TYPE_WRITE);
   if (!pte.a || (!pte.d && is_write)) {
     pte.a = true;
     pte.d |= is_write;

commit 5f02f29ad1e9d164fea950609f64ae8fb983d997

riscv64,system,mmu: fix sign bug introduced in c3285ed6a

src/isa/riscv64/system/mmu.c
@@ -79,10 +79,10 @@ static paddr_t ptw(vaddr_t vaddr, int type) {
   word_t p_pte; // pte pointer
   PTE pte;
   int level;
-  uint64_t vaddr39 = vaddr << (64 - 39);
+  int64_t vaddr39 = vaddr << (64 - 39);
   vaddr39 >>= (64 - 39);
   bool is_write = (type == MEM_TYPE_WRITE);
-  if (vaddr39 != vaddr) goto bad;
+  if ((uint64_t)vaddr39 != vaddr) goto bad;
   for (level = PTW_LEVEL - 1; level >= 0;) {
     p_pte = pg_base + VPNi(vaddr, level) * PTE_SIZE;
     pte.val	= paddr_read(p_pte, PTE_SIZE);