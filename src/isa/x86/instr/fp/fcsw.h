6d45321316543eaf84685841ca1d69891ab32d3b
Zihao Yu <yuzihao@ict.ac.cn>
2021-07-12 00:45:44 +0800

    x86,instr,fp: fix the shape of curve

--- src/isa/x86/instr/fp/fcsw.h
+++ src/isa/x86/instr/fp/fcsw.h
@@ -1,17 +1,18 @@
 def_EHelper(fxam) {
-  // TODO
+  rtl_li(s, s0, ((int64_t)*dfdest < 0) << 9); // sign bit
+  rtl_li(s, s1, 0x4 << 8); // normal number
+  rtl_or(s, &cpu.fsw, s0, s1);
 }
 
 def_EHelper(fnstsw) {
-  // TODO
-  rtl_li(s, s0, 0);
-  rtl_sr(s, R_AX, s0, 2);
+  rtl_sr(s, R_AX, &cpu.fsw, 2);
 }
 
 def_EHelper(fnstcw) {
-  // TODO
   rt_decode_mem(s, id_dest, false, 0);
-  rtl_li(s, s0, 0);
+  uint32_t x86_fp_get_rm();
+  uint32_t rm_x86 = x86_fp_get_rm();
+  rtl_li(s, s0, rm_x86 << 10);
   rtl_sm(s, s0, &s->isa.mbr, s->isa.moff, 2, MMU_DYNAMIC);
 }
 
@@ -19,15 +20,7 @@ def_EHelper(fldcw) {
   rt_decode_mem(s, id_dest, false, 0);
   rtl_lm(s, s0, &s->isa.mbr, s->isa.moff, 2, MMU_DYNAMIC);
 
-  int rm_x86 = BITS(*s0, 11, 10);
-  int rm_fpcall = 0;
-  switch (rm_x86) {
-    case 0b00: rm_fpcall = FPCALL_RM_RNE; break;
-    case 0b01: rm_fpcall = FPCALL_RM_RDN; break;
-    case 0b10: rm_fpcall = FPCALL_RM_RUP; break;
-    case 0b11: rm_fpcall = FPCALL_RM_RTZ; break;
-  }
-
-  void fp_set_rm(uint32_t rm);
-  fp_set_rm(rm_fpcall);
+  uint32_t rm_x86 = BITS(*s0, 11, 10);
+  void x86_fp_set_rm(uint32_t rm_x86);
+  x86_fp_set_rm(rm_x86);
 }
