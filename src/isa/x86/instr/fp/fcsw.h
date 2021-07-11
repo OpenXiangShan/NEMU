b3d231968305e8375189727364064bfa74d3ddeb
Zihao Yu <yuzihao@ict.ac.cn>
2021-07-11 23:04:24 +0800

    x86,instr,fp: fix the shape of taiji

--- src/isa/x86/instr/fp/fcsw.h
+++ src/isa/x86/instr/fp/fcsw.h
@@ -16,5 +16,18 @@ def_EHelper(fnstcw) {
 }
 
 def_EHelper(fldcw) {
-  // TODO
+  rt_decode_mem(s, id_dest, false, 0);
+  rtl_lm(s, s0, &s->isa.mbr, s->isa.moff, 2, MMU_DYNAMIC);
+
+  int rm_x86 = BITS(*s0, 11, 10);
+  int rm_fpcall = 0;
+  switch (rm_x86) {
+    case 0b00: rm_fpcall = FPCALL_RM_RNE; break;
+    case 0b01: rm_fpcall = FPCALL_RM_RDN; break;
+    case 0b10: rm_fpcall = FPCALL_RM_RUP; break;
+    case 0b11: rm_fpcall = FPCALL_RM_RTZ; break;
+  }
+
+  void fp_set_rm(uint32_t rm);
+  fp_set_rm(rm_fpcall);
 }
--- src/isa/x86/instr/fp/fp.c
+++ src/isa/x86/instr/fp/fp.c
@@ -3,8 +3,14 @@
 #include <cpu/decode.h>
 #include <cpu/cpu.h>
 
+static uint32_t g_rm = FPCALL_RM_RNE;
+
+void fp_set_rm(uint32_t rm) {
+  g_rm = rm;
+}
+
 uint32_t isa_fp_get_rm(Decode *s) {
-  return FPCALL_RM_RNE;
+  return g_rm;
 }
 
 void isa_fp_set_ex(uint32_t ex) {
