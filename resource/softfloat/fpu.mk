d066d65f5c1fabd3fbbf277eaae506d961cbfeda
Zihao Yu <yuzihao@ict.ac.cn>
2021-07-25 15:27:30 +0800

    Makefile: rename INC_DIR to INC_PATH to keep consistent with AM

--- resource/softfloat/fpu.mk
+++ resource/softfloat/fpu.mk
@@ -1,8 +1,8 @@
 ifdef CONFIG_FPU_SOFT
 SOFTFLOAT = resource/softfloat/build/softfloat.a
 SPECIALIZE_TYPE = $(if $(CONFIG_ISA_riscv64),RISCV,8086-SSE)
-INC_DIR += resource/softfloat/repo/source/include
-INC_DIR += resource/softfloat/repo/source/$(SPECIALIZE_TYPE)
+INC_PATH += resource/softfloat/repo/source/include
+INC_PATH += resource/softfloat/repo/source/$(SPECIALIZE_TYPE)
 ARCHIVES += $(SOFTFLOAT)
 $(SOFTFLOAT):
 	SPECIALIZE_TYPE=$(SPECIALIZE_TYPE) $(MAKE) -s -C resource/softfloat/
--- src/isa/filelist.mk
+++ src/isa/filelist.mk
@@ -1,2 +1,2 @@
-INC_DIR += $(NEMU_HOME)/src/isa/$(GUEST_ISA)/include
+INC_PATH += $(NEMU_HOME)/src/isa/$(GUEST_ISA)/include
 DIRS-y += src/isa/$(GUEST_ISA)
