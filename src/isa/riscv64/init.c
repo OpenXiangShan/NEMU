a3431ce91e15f368bd0dc01ffe0375a859aa491b
Zihao Yu <yuzihao@ict.ac.cn>
2021-08-03 22:45:58 +0800

    riscv64,init: fix default img
    * The origin instruction `lui t0,0x80000` will yield 0xffffffff80000000
      in riscv64, which causes illegal memory access in the new version of
      QEMU.
    * This bug is not exposed in QEMU 3.2.0. It seems that such version of
      QEMU does not report illegal memory access with 0xffffffff80000000.

--- src/isa/riscv64/init.c
+++ src/isa/riscv64/init.c
@@ -3,10 +3,11 @@
 #include "local-include/csr.h"
 
 static const uint32_t img [] = {
-  0x800002b7,  // lui t0,0x80000
-  0x0002a023,  // sw  zero,0(t0)
-  0x0002a503,  // lw  a0,0(t0)
+  0x00000297,  // auipc t0,0
+  0x0002b823,  // sw  zero,16(t0)
+  0x0102b503,  // lw  a0,16(t0)
   0x0000006b,  // nemu_trap
+  0xdeadbeef,  // some data
 };
 
 void init_csr();
