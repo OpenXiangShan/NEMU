2aa1e19c1715a86811a4a75eddc1321128df5aad
Zihao Yu <yuzihao@ict.ac.cn>
2021-08-10 20:21:36 +0800

    cpu,difftest: rename arguments in prototype

--- include/cpu/difftest.h
+++ include/cpu/difftest.h
@@ -20,8 +20,8 @@ static inline void difftest_detach() {}
 static inline void difftest_attach() {}
 #endif
 
-extern void (*ref_difftest_memcpy)(paddr_t dest, void *src, size_t n, bool direction);
-extern void (*ref_difftest_regcpy)(void *c, bool direction);
+extern void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction);
+extern void (*ref_difftest_regcpy)(void *dut, bool direction);
 extern void (*ref_difftest_exec)(uint64_t n);
 extern void (*ref_difftest_raise_intr)(uint64_t NO);
 
