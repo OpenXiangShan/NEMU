d9c309015b3bd2647b0c3dd2fb0d638602e33789
Zihao Yu <yuzihao@ict.ac.cn>
2021-08-15 16:45:35 +0800

    scripts,git.mk: update year

--- scripts/git.mk
+++ scripts/git.mk
@@ -1,9 +1,9 @@
-STUID = 191220000
+STUID = 201220000
 STUNAME = 张三
 
 # DO NOT modify the following code!!!
 
-GITFLAGS = -q --author='tracer-ics2020 <tracer@njuics.org>' --no-verify --allow-empty
+GITFLAGS = -q --author='tracer-ics2021 <tracer@njuics.org>' --no-verify --allow-empty
 
 #ifndef __ICS_EXPORT
 ifdef __NOT_DEFINED
