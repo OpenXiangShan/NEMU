405ae2ab35876b97738b2fd54d0e202fc51ba608
Zihao Yu <yuzihao@ict.ac.cn>
2021-08-16 14:28:58 +0800

    scripts: remove unused file

--- scripts/isa.mk
+++
@@ -1,6 +0,0 @@
-GUEST_ISA ?= riscv32
-ISAS = $(shell ls $(NEMU_HOME)/src/isa/)
-ifeq ($(filter $(ISAS), $(GUEST_ISA)), ) # GUEST_ISA must be valid
-$(error Invalid GUEST_ISA=$(GUEST_ISA). Supported: $(ISAS))
-endif
-NAME := $(GUEST_ISA)-$(NAME)
