a9d0ad6da554ebfd1951a925b4ed3682bf6421bf
Zihao Yu <yuzihao@ict.ac.cn>
2021-09-25 10:52:36 +0800

    utils,ftrace: support multiple ELF files

--- src/utils/ftrace.c
+++ src/utils/ftrace.c
@@ -2,11 +2,12 @@
 #include <elf.h>
 
 #if defined(CONFIG_FTRACE_COND)
-static char *strtab = NULL;
-static Elf32_Sym *symtab = NULL;
-static int nr_symtab_entry = 0;
+static char **strtab = NULL;
+static Elf32_Sym **symtab = NULL;
+static int *nr_symtab_entry = NULL;
+static int nr_file = 0;
 
-void init_ftrace(const char *file) {
+static void read_elf(const char *file, int n) {
   FILE *fp = fopen(file, "rb");
   Assert(fp, "Can not open '%s'", file);
 
@@ -41,18 +42,18 @@ void init_ftrace(const char *file) {
     if (sh[i].sh_type == SHT_SYMTAB &&
         strcmp(shstrtab + sh[i].sh_name, ".symtab") == 0) {
       /* Load symbol table from file */
-      symtab = malloc(sh[i].sh_size);
+      symtab[n] = malloc(sh[i].sh_size);
       fseek(fp, sh[i].sh_offset, SEEK_SET);
-      ret = fread(symtab, sh[i].sh_size, 1, fp);
+      ret = fread(symtab[n], sh[i].sh_size, 1, fp);
       assert(ret == 1);
-      nr_symtab_entry = sh[i].sh_size / sizeof(symtab[0]);
+      nr_symtab_entry[n] = sh[i].sh_size / sizeof(symtab[n][0]);
     }
     else if (sh[i].sh_type == SHT_STRTAB &&
         strcmp(shstrtab + sh[i].sh_name, ".strtab") == 0) {
       /* Load string table from file */
-      strtab = malloc(sh[i].sh_size);
+      strtab[n] = malloc(sh[i].sh_size);
       fseek(fp, sh[i].sh_offset, SEEK_SET);
-      ret = fread(strtab, sh[i].sh_size, 1, fp);
+      ret = fread(strtab[n], sh[i].sh_size, 1, fp);
       assert(ret == 1);
     }
   }
@@ -60,7 +61,7 @@ void init_ftrace(const char *file) {
   free(sh);
   free(shstrtab);
 
-  assert(strtab != NULL && symtab != NULL);
+  assert(strtab[n] != NULL && symtab[n] != NULL);
 
   fclose(fp);
 }
@@ -70,23 +71,49 @@ static int depth = 0;
 static const char* find_fun_name(word_t pc, bool exact) {
   static const char not_found[] = "???";
 
-  int i;
+  int i, n;
   if (exact) {
-    for (i = 0; i < nr_symtab_entry; i ++) {
-      if (ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC && pc == symtab[i].st_value) {
-        return strtab + symtab[i].st_name;
+    for (n = 0; n < nr_file; n ++) {
+      for (i = 0; i < nr_symtab_entry[n]; i ++) {
+        if (ELF32_ST_TYPE(symtab[n][i].st_info) == STT_FUNC && pc == symtab[n][i].st_value) {
+          return strtab[n] + symtab[n][i].st_name;
+        }
       }
     }
   } else {
-    for (i = 0; i < nr_symtab_entry; i ++) {
-      if (ELF32_ST_TYPE(symtab[i].st_info) == STT_FUNC &&
-          pc >= symtab[i].st_value && pc < symtab[i].st_value + symtab[i].st_size) {
-        return strtab + symtab[i].st_name;
+    for (n = 0; n < nr_file; n ++) {
+      for (i = 0; i < nr_symtab_entry[n]; i ++) {
+        if (ELF32_ST_TYPE(symtab[n][i].st_info) == STT_FUNC &&
+            pc >= symtab[n][i].st_value && pc < symtab[n][i].st_value + symtab[n][i].st_size) {
+          return strtab[n] + symtab[n][i].st_name;
+        }
       }
     }
   }
 
   return not_found;
+}
+
+void init_ftrace(const char *files) {
+  nr_file = 1;
+  const char *p = files;
+  while ((p = strchr(p, ',')) != NULL) { nr_file ++; p ++; }
+
+  strtab = malloc(nr_file * sizeof(*strtab));
+  symtab = malloc(nr_file * sizeof(*symtab));
+  nr_symtab_entry = malloc(nr_file * sizeof(*nr_symtab_entry));
+
+  char *file = strdup(files);
+  char *file_end = file;
+  char *file_save = file;
+  int i;
+  for (i = 0; i < nr_file; i ++) {
+    file_end = strchr(file, ',');
+    if (file_end != NULL) *file_end = '\0';
+    read_elf(file, i);
+    file = file_end + 1;
+  }
+  free(file_save);
 }
 
 void ftrace_call(word_t pc, word_t target) {
