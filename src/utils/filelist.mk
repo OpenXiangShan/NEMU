24152312fd9d773e329c57a2c8fc94fb74ceee3f
Zihao Yu <yuzihao@ict.ac.cn>
2021-09-28 19:36:36 +0800

    utils,disasm: bind with CONFIG_ITRACE

--- src/utils/filelist.mk
+++ src/utils/filelist.mk
@@ -1,4 +1,4 @@
-ifdef CONFIG_TARGET_NATIVE_ELF
+ifdef CONFIG_ITRACE
 CXXSRC = src/utils/disasm.cc
 CXXFLAGS += $(shell llvm-config-11 --cxxflags) -fPIE
 LIBS += $(shell llvm-config-11 --libs)
--- src/utils/iqueue.c
+++ src/utils/iqueue.c
@@ -24,7 +24,7 @@ void iqueue_dump() {
   printf("======== instruction queue =========\n");
   for (i = 0; i < INSTR_QUEUE_SIZE; i ++) {
     printf("%5s " FMT_WORD ": ", (i == victim_idx ? "-->" : ""), iqueue[i].pc);
-#ifdef CONFIG_TARGET_NATIVE_ELF
+#ifdef CONFIG_ITRACE
     void disassemble(char *str, int size, vaddr_t pc, uint8_t *code, int nbyte);
     char buf[80];
     disassemble(buf, sizeof(buf), iqueue[i].pc, iqueue[i].instr, iqueue[i].ilen);
