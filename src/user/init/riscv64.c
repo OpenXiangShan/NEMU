1d240ede8e829cc784d222f431d6bea66859da18
Zihao Yu <yuzihao@ict.ac.cn>
2021-09-29 18:51:54 +0800

    user,init,riscv64: remove unused code

--- src/user/init/riscv64.c
+++ src/user/init/riscv64.c
@@ -3,8 +3,6 @@
 void init_csr();
 
 void isa_init_user(word_t sp) {
-  init_csr();
-  cpu.mode = MODE_U;
+  init_csr(); // initialize CSRs for floating point
   cpu.gpr[2]._64 = sp;
-  //cpu.edx = 0; // no handler for atexit()
 }
