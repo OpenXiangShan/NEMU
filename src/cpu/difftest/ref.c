17f5e78a1d0f9e4119964ea318fed59a841da70e
William Wang <zeweiwang@outlook.com>
2021-10-08 17:33:44 +0800

    Revert "Merge pull request #29 from OpenXiangShan/revert-28-run-ahead"
    This reverts commit ecde1fecf426c3605c982c7c5346bb637f9c8521, reversing
    changes made to 69b70c668b732358f7e8035c562292a47168cd2c.

--- src/cpu/difftest/ref.c
+++ src/cpu/difftest/ref.c
@@ -70,6 +70,12 @@ void difftest_guided_exec(void * guide) {
 }
 #endif
 
+#ifdef CONFIG_QUERY_REF
+void difftest_query_ref(void * result_buffer, uint64_t type) {
+  isa_difftest_query_ref(result_buffer, type);
+}
+#endif
+
 void difftest_raise_intr(word_t NO) {
   isa_difftest_raise_intr(NO);
 }
---
+++ src/utils/dynamic-config.c
@@ -0,0 +1,8 @@
+#include <common.h>
+
+struct DynamicConfig dynamic_config = {};
+void update_dynamic_config(void* config) {
+  memcpy((void*)&dynamic_config, config, sizeof(struct DynamicConfig));
+  printf("NEMU dynamic_config update\n");
+  printf("  - ignore_illegal_mem_access %x\n", dynamic_config.ignore_illegal_mem_access);
+}
