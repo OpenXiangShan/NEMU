0cdf783802f7b6dc9b078629386604772d0765d6
Zihao Yu <yuzihao@ict.ac.cn>
2021-10-10 18:40:54 +0800

    x86,instr,arith: fix missing code to load immediate for imul3

--- src/isa/x86/instr/arith.h
+++ src/isa/x86/instr/arith.h
@@ -390,6 +390,7 @@ def_EHelper(imul2) {
 // imul with three operands
 def_EHelper(imul3) {
   rtl_decode_binary(s, false, true); // the 3rd operand is always immediate
+  rt_decode(s, id_src2, true, s->isa.width);
   rtl_sext(s, dsrc1, dsrc1, s->isa.width);
 
   int need_update_eflags = MUXDEF(CONFIG_x86_CC_SKIP, s->isa.flag_def != 0, true);
