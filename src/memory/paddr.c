18099f90c191f668e6c5af8347b134ef61e03467
William Wang <zeweiwang@outlook.com>
2021-10-22 18:17:33 +0800

    chore: fix debug paddr print fmt

--- src/memory/paddr.c
+++ src/memory/paddr.c
@@ -98,7 +98,7 @@ word_t paddr_read(paddr_t addr, int len, int type, int mode) {
 void paddr_write(paddr_t addr, int len, word_t data, int mode) {
 #ifdef CONFIG_SHARE
   if(dynamic_config.debug_difftest) {
-    fprintf(stderr, "[NEMU]  paddr write addr:%lx len:%d mode:%d\n", addr, len, mode);
+    fprintf(stderr, "[NEMU]  paddr write addr:" FMT_PADDR " len:%d mode:%d\n", addr, len, mode);
   }
 #endif
 
