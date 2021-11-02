70df9ece39a0b56d919d8753ea84686d2c43a147
Zihao Yu <yuzihao@ict.ac.cn>
2021-11-02 12:14:26 +0800

    riscv64,inst: pass fceux

--- src/isa/riscv64/instr/decode.c
+++ src/isa/riscv64/instr/decode.c
@@ -49,7 +49,7 @@ def_THelper(main) {
   return table_inv(s);
 };
 
-int isa_fetch_decode(Decode *s) {
+int isa_old_fetch_decode(Decode *s) {
   int idx = EXEC_ID_inv;
 
   s->isa.instr.val = instr_fetch(&s->snpc, 2);
