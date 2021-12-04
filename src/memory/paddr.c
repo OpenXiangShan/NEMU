da2b003a9612e4aa9d8851c7b4a2208d2a07e4d4
wangkaifan <wangkaifan@ict.ac.cn>
2021-12-04 10:12:51 +0800

    misc: use FMT_PADDR to dump paddr

--- src/memory/paddr.c
+++ src/memory/paddr.c
@@ -60,7 +60,7 @@ void init_mem() {
 word_t paddr_read(paddr_t addr, int len, int type, int mode) {
 #ifdef CONFIG_SHARE
   if(dynamic_config.debug_difftest) {
-    fprintf(stderr, "[NEMU]  paddr read addr:%lx len:%d type:%d mode:%d\n", addr, len, type, mode);
+    fprintf(stderr, "[NEMU]  paddr read addr:" FMT_PADDR " len:%d type:%d mode:%d\n", addr, len, type, mode);
   }
 #endif
 
@@ -98,7 +98,7 @@ word_t paddr_read(paddr_t addr, int len, int type, int mode) {
 void paddr_write(paddr_t addr, int len, word_t data, int mode) {
 #ifdef CONFIG_SHARE
   if(dynamic_config.debug_difftest) {
-    fprintf(stderr, "[NEMU]  paddr write addr:%lx len:%d mode:%d\n", addr, len, mode);
+    fprintf(stderr, "[NEMU]  paddr write addr:" FMT_PADDR " len:%d mode:%d\n", addr, len, mode);
   }
 #endif
 
