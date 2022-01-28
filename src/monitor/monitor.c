dfe9a4bb1ffd14d6f8ae84755bc86ff87677de4a
Chen Lu <181250012@smail.nju.edu.cn>
2022-01-28 12:17:48 +0800

    monitor: make variable img_file global

--- src/monitor/monitor.c
+++ src/monitor/monitor.c
@@ -33,7 +33,7 @@ void sdb_set_batch_mode();
 
 static char *log_file = NULL;
 static char *diff_so_file = NULL;
-static char *img_file = NULL;
+char *img_file = NULL;
 static int difftest_port = 1234;
 
 #ifndef CONFIG_MODE_USER
