5a6ea55a40d5917c19841842f44f7335dcd1d309
Zhou Yaoyang <shinezyy@qq.com>
2022-04-04 16:09:32 +0800

    gcpt: [wip] add simpoint profiling, taking cpt, and restoring cpt
    - Compiling cpp files together and expose C APIs
    - c++17, libboost, and zlib are required

---
+++ include/checkpoint/cpt_env.h
@@ -0,0 +1,12 @@
+#ifndef __CHECKPOINT_CPT_ENV__
+#define __CHECKPOINT_CPT_ENV__
+
+extern char *output_base_dir;
+extern char *config_name;
+extern char *workload_name;
+extern char *simpoints_dir;
+extern int cpt_id;
+extern char *cpt_file;
+extern char *restorer;
+
+#endif
