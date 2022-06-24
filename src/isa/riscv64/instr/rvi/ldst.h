3171ec552e646c8921776d20e960ba19e07ab5e9
Zihao Yu <yuzihao@ict.ac.cn>
2022-06-24 13:23:36 +0800

    add license

--- src/isa/riscv64/instr/rvi/ldst.h
+++ src/isa/riscv64/instr/rvi/ldst.h
@@ -1,3 +1,18 @@
+/***************************************************************************************
+* Copyright (c) 2014-2021 Zihao Yu, Nanjing University
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
 #define def_ldst_template(name, rtl_instr, width, mmu_mode) \
   def_EHelper(name) { \
     concat(rtl_, rtl_instr) (s, ddest, dsrc1, id_src2->imm, width, mmu_mode); \
