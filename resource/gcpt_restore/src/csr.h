8ccb95e2d43e3c4ccc758510620652ae68efd92c
JiaXiaoyu <xyyy1420@gmail.com>
2023-09-04 13:12:20 +0800

    fix: allow restore rvh csr reg to be selectively turned on using the rvh flag
    Signed-off-by: JiaXiaoyu <xyyy1420@gmail.com>

--- resource/gcpt_restore/src/csr.h
+++ resource/gcpt_restore/src/csr.h
@@ -16,6 +16,8 @@
 #ifndef __CSR_H__
 #define __CSR_H__
 
+#include <autoconf.h>
+
 //no mhartid here
 
 #define CSRS(f) \
@@ -32,7 +34,14 @@
   f(stvec      , 0x105) f(scounteren , 0x106) \
   f(sscratch   , 0x140) f(sepc       , 0x141) f(scause     , 0x142) \
   f(stval      , 0x143) \
-  f(satp       , 0x180) \
+  f(satp       , 0x180)
+
+#define NOP \
+  addi x0, x0, 0;
+
+#ifdef CONFIG_RVH
+
+#define HCSRS(f) \
   f(hstatus    , 0x600) f(hedeleg    , 0x602) f(hideleg    , 0x603) \
   f(hie        , 0x604) f(hcounteren , 0x606) f(hgeie      , 0x607) \
   f(htval      , 0x643) f(hip        , 0x644) f(hvip       , 0x645) \
@@ -42,8 +51,16 @@
   f(vsscratch  , 0x240) f(vsepc      , 0x241) f(vscause    , 0x242) \
   f(vstval     , 0x243) f(vsip       , 0x244) f(vsatp      , 0x280) \
   f(mtval2     , 0x34b) f(mtinst     , 0x34A)
+#else
+#define HCSRS(f) NOP;
+#endif // CONFIG_RVH
 
 
+#ifdef CONFIG_RVV
+#define VCSRS(f) NOP;
+#else
+#define VCSRS(f) NOP;
+#endif // CONFIG_RVV
 
 #define CSRS_RESTORE(name, addr) \
   li t2, addr; \
