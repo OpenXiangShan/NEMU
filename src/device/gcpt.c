2f598896edb52842ed7344fcfc9e4de002009da0
jiaxiaoyu <xyyy1420@gmail.com>
2024-03-11 17:20:53 +0800

    finish zstd/gz checkpoint using gcpt device and restore from device
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/device/gcpt.c
+++ src/device/gcpt.c
@@ -1,15 +1,14 @@
 #include "device/map.h"
 #include <utils.h>
 
-#define CONFIG_GCPT_MMIO 50000000
+#define CONFIG_GCPT_MMIO 0x60000000
 #define GCPT_MEM_SIZE (128 * 1024 * 1024)
 
-static uint8_t *gcpt_host_address = NULL;
-
 // read gcpt memory do not need any handle, just resturn data
-static void gcpt_io_handler(uint32_t offset, int len, bool is_write) {}
+static void gcpt_io_handler(uint32_t offset, int len, bool is_write) {
+}
 
-struct {
+static struct {
   uint8_t *mmio_address;
   uint8_t *host_address;
   uint64_t size;
@@ -28,7 +27,6 @@ uint64_t get_gcpt_mmio_size(){
 }
 
 void init_gcpt() {
-  gcpt_host_address = new_space(gcpt_info.size);
-  gcpt_info.host_address=gcpt_host_address;
+  gcpt_info.host_address = new_space(gcpt_info.size);
   add_mmio_map("gcpt", (uint64_t)gcpt_info.mmio_address, gcpt_info.host_address, gcpt_info.size, gcpt_io_handler);
 }
