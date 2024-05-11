47ac4e2dadf961ac81c59a2044ca0ac231f0ed08
Yinan Xu <xuyinan@ict.ac.cn>
2024-05-11 21:24:15 +0800

    system: remove the extra printf (#326)

--- src/isa/riscv64/instr/priv/system.c
+++ src/isa/riscv64/instr/priv/system.c
@@ -44,7 +44,6 @@ int rtl_sys_slow_path(Decode *s, rtlreg_t *dest, const rtlreg_t *src1, uint32_t 
       // Please keep the following lines same as in src/isa/riscv64/instr/special.h.
       rtl_hostcall(s, HOSTCALL_EXIT, NULL, &cpu.gpr[10]._64, NULL, 0); // gpr[10] is $a0
       longjmp_exec(NEMU_EXEC_END);
-      printf("hahaha\n");
 #endif
     } else {
       rtl_hostcall(s, HOSTCALL_PRIV, jpc, src1, NULL, id);
