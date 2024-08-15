9e7283e66ac91ad7217fb609888b75ada1ebb694
Xu, Zefan <ceba_robot@outlook.com>
2024-08-15 13:53:37 +0800

    ci: all compilation test are in one job
    There are some compilation tests in different jobs, which are moved in compilation test now. This patch also improve the log of compilation test.

--- scripts/compilation-test.sh
+++ scripts/compilation-test.sh
@@ -1,46 +1,55 @@
 #!/bin/bash
 
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-NC='\033[0m'
+INFO="\033[1m"
+ERROR="\033[1;31m"
+GOOD="\033[1;32m"
+NC="\033[0m"
 
 failed_configs=()
+
+if [ ! -f "$NEMU_HOME/src/nemu-main.c" ]; then
+  echo -e "${ERROR}NEMU_HOME=$NEMU_HOME is not a NEMU repo${NC}"
+  exit 1
+fi
+
+echo -e "${INFO}Start compilation test for all configurations${NC}"
+make clean-all
 
 for config_path in configs/riscv64-*defconfig; do
     if [[ -f "$config_path" ]]; then
         config=$(basename "$config_path")
-        echo "Building configuration: $config"
-
-        make clean
+        echo "::group::$config"
+        echo -e "${INFO}Building configuration: $config${NC}"
         
-        # 执行 make 命令
-        make "$config"
+        make $config
         if [ $? -ne 0 ]; then
-            echo "Failed to execute 'make $config'"
-            exit 1
+            echo -e "${ERROR}Failed to setup configuration $config${NC}"
+            failed_configs+=("$config")
+            continue
         fi
         
         make -j8
         if [ $? -ne 0 ]; then
-            echo "Compilation failed for configuration: $config"
+            echo -e "${ERROR}Compilation failed for configuration: $config${NC}"
             failed_configs+=("$config")
             continue
         fi
 
-        echo "Successfully built: $config"
+        echo -e "${GOOD}Successfully built: $config${NC}"
+        echo "::endgroup::"
     else
-        echo "No riscv64-*defconfig files found in configs directory."
+        echo -e "${ERROR}No riscv64-*defconfig files found in configs directory.${NC}"
         exit 1
     fi
 done
 
 if [ ${#failed_configs[@]} -ne 0 ]; then
-    echo "The following configurations failed to build:"
+    echo -e "${ERROR}The following configurations failed to build:${NC}"
     for failed_config in "${failed_configs[@]}"; do
         echo "$failed_config"
     done
     exit 1
 else
-    echo "All configurations built successfully."
+    echo -e "${GOOD}All configurations built successfully.${NC}"
     exit 0
 fi
