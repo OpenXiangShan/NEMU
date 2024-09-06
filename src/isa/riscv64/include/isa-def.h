e995f9e1a921ae15ac4d0bbefa153e664b55ad7d
Xu, Zefan <ceba_robot@outlook.com>
2024-09-06 10:34:52 +0800

    fix: modify ifdef

--- src/isa/riscv64/include/isa-def.h
+++ src/isa/riscv64/include/isa-def.h
@@ -289,9 +289,7 @@ enum { MODE_U = 0, MODE_S, MODE_HS, MODE_M };
 int get_data_mmu_state();
 #ifdef CONFIG_RVH
 int get_hyperinst_mmu_state();
-#define isa_mmu_state() get_data_mmu_state() 
-#else
-#define isa_mmu_state() get_data_mmu_state()
 #endif //CONFIG_RVH
 
+#define isa_mmu_state() get_data_mmu_state() 
 #endif
