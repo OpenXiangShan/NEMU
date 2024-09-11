ffad0dda9b000bb2ca18b0f829238fd1d896bbe1
Xu, Zefan <ceba_robot@outlook.com>
2024-09-11 10:56:55 +0000

    feat(misaligned): add config for vector misaligned detect
    XiangShan support misaligned memory access for normal load/store but not for vector load/store, while spike could only enable misaligned memory access for all load/store. So this patch adds a config to determine whether vector misaligned memory access is allowed.
    
    Also, this patch aligns the misaligned memory access config among riscv64-xs.

--- src/isa/riscv64/system/mmu.c
+++ src/isa/riscv64/system/mmu.c
@@ -705,10 +705,12 @@ void isa_misalign_data_addr_check(vaddr_t vaddr, int len, int type) {
 void isa_vec_misalign_data_addr_check(vaddr_t vaddr, int len, int type) {
   if (unlikely((vaddr & (len - 1)) != 0)) {
     Logm("addr misaligned happened: vaddr:%lx len:%d type:%d pc:%lx", vaddr, len, type, cpu.pc);
-    int ex = cpu.amo || type == MEM_TYPE_WRITE ? EX_SAM : EX_LAM;
-    IFDEF(CONFIG_USE_XS_ARCH_CSRS, vaddr = INTR_TVAL_SV48_SEXT(vaddr));
-    INTR_TVAL_REG(ex) = vaddr;
-    longjmp_exception(ex);
+    if (ISDEF(CONFIG_VECTOR_AC_SOFT)) {
+      int ex = cpu.amo || type == MEM_TYPE_WRITE ? EX_SAM : EX_LAM;
+      IFDEF(CONFIG_USE_XS_ARCH_CSRS, vaddr = INTR_TVAL_SV48_SEXT(vaddr));
+      INTR_TVAL_REG(ex) = vaddr;
+      longjmp_exception(ex);
+    }
   }
 }
 
