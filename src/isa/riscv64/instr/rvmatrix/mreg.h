0bafa623f2635e01c42e7003d341e751aa5d658e
Cai Luoshan <cailuoshan18@mails.ucas.ac.cn>
2024-11-28 19:12:33 +0800

    RV64Matrix: Support Matrix Extension Version 0.3, Config & LdSt Instructions (first edition)

---
+++ src/isa/riscv64/instr/rvmatrix/mreg.h
@@ -0,0 +1,60 @@
+/***************************************************************************************
+* Copyright (c) 2020-2022 Institute of Computing Technology, Chinese Academy of Sciences
+*
+* NEMU is licensed under Mulan PSL v2.
+* You can use this software according to the terms and conditions of the Mulan PSL v2.
+* You may obtain a copy of Mulan PSL v2 at:
+*          http://license.coscl.org.cn/MulanPSL2
+*
+* THIS SOFTWARE IS PROVIDED ON AN "AS IS" BASIS, WITHOUT WARRANTIES OF ANY KIND,
+* EITHER EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO NON-INFRINGEMENT,
+* MERCHANTABILITY OR FIT FOR A PARTICULAR PURPOSE.
+*
+* See the Mulan PSL v2 for more details.
+***************************************************************************************/
+
+#ifdef CONFIG_RVMATRIX
+#ifndef __RISCV64_MREG_H__
+#define __RISCV64_MREG_H__
+
+#include "common.h"
+
+#define MLEN 1024
+#define MRLEN 128
+#define MELEN 64
+#define MAMUL 8
+#define MLENB (MLEN/8)
+
+#define MRNUM   (MLEN/MRLEN)
+#define MRENUM64 (MRLEN/64)
+#define MRENUM32 (MRLEN/32)
+#define MRENUM16 (MRLEN/16)
+#define MRENUM8  (MRLEN/8)
+
+#define TMMAX 8   // MLEN/RLEN=8
+#define TKMAX 2   // min{MLEN/RLEN, RLEN/SEW}, SEW=8/16/32/64, init_SEW=ELEN
+#define TNMAX 2   // RLEN/SEW
+
+static inline int check_mtreg_num(int num) {
+  assert(num >= 0 && num < 8);
+  return num;
+}
+
+static inline int check_mtreg_row(int row) {
+  assert(row >= 0 && row < MRNUM);
+  return row;
+}
+
+static inline int check_mtreg_idx(int idx, int elen) {
+  assert(idx >= 0 && idx < MRLEN/elen);
+  return idx;
+}
+
+#define mtreg_l64(num, row, idx) (cpu.mtr[check_mtreg_num(num)][check_mtreg_row(row)]._64[check_mtreg_idx(idx, 64)])
+#define mtreg_l32(num, row, idx) (cpu.mtr[check_mtreg_num(num)][check_mtreg_row(row)]._32[check_mtreg_idx(idx, 32)])
+#define mtreg_l16(num, row, idx) (cpu.mtr[check_mtreg_num(num)][check_mtreg_row(row)]._16[check_mtreg_idx(idx, 16)])
+#define mtreg_l8(num, row, idx) (cpu.mtr[check_mtreg_num(num)][check_mtreg_row(row)]._8[check_mtreg_idx(idx, 8)])
+
+#endif //__RISCV64_MREG_H__
+
+#endif // CONFIG_RVMATRIX
