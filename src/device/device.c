f476a7a238826a20bf17352247d8bce5e6df6173
jiaxiaoyu <xyyy1420@gmail.com>
2024-12-06 15:18:53 +0800

    feat(libcheckpoint): add support for libcheckpoint
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/device/device.c
+++ src/device/device.c
@@ -36,7 +36,6 @@ void init_disk();
 void init_sdcard();
 void init_flash();
 void load_flash_contents(const char *);
-void init_gcpt();
 
 void send_key(uint8_t, bool);
 void vga_update_screen();
@@ -106,6 +105,5 @@ void init_device() {
   IFDEF(CONFIG_HAS_FLASH, load_flash_contents(CONFIG_FLASH_IMG_PATH));
   add_alarm_handle(set_device_update_flag);
   init_alarm();
-  init_gcpt();
 #endif
 }
