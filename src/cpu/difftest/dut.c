ad9e9164c4e5a83d6c170a454116f6cb78c1fb24
jiaxiaoyu <xyyy1420@gmail.com>
2024-12-17 10:36:28 +0800

    fix(difftest attach): add pmp and pmp cfg copy to difftest attach
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/cpu/difftest/dut.c
+++ src/cpu/difftest/dut.c
@@ -24,6 +24,8 @@
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_pmpcpy)(void *dut, bool direction) = NULL;
+void (*ref_difftest_pmp_cfg_cpy)(void *dut, bool direction) = NULL;
 void (*ref_difftest_exec)(uint64_t n) = NULL;
 void (*ref_difftest_raise_intr)(uint64_t NO) = NULL;
 void (*ref_difftest_dirty_fsvs)(const uint64_t dirties) = NULL;
@@ -99,6 +101,12 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
   void (*ref_difftest_init)(int) = dlsym(handle, "difftest_init");
   assert(ref_difftest_init);
 
+  ref_difftest_pmpcpy = dlsym(handle, "difftest_pmpcpy");
+  assert(ref_difftest_pmpcpy);
+
+  ref_difftest_pmp_cfg_cpy = dlsym(handle, "difftest_pmp_cfg_cpy");
+  assert(ref_difftest_pmp_cfg_cpy);
+
 #ifdef CONFIG_DIFFTEST_REF_SPIKE
   ref_difftest_store_commit = dlsym(handle, "difftest_store_commit");
   assert(ref_difftest_store_commit);
@@ -111,15 +119,11 @@ void init_difftest(char *ref_so_file, long img_size, int port) {
 
   ref_difftest_init(port);
   ref_difftest_memcpy(RESET_VECTOR, guest_to_host(RESET_VECTOR), img_size, DIFFTEST_TO_REF);
-#ifdef CONFIG_HAS_FLASH
-  ref_difftest_memcpy(CONFIG_FLASH_START_ADDR, flash_base, CONFIG_FLASH_SIZE, DIFFTEST_TO_REF);
-#endif
   ref_difftest_regcpy(&cpu, DIFFTEST_TO_REF);
 }
 
 static void checkregs(CPU_state *ref, vaddr_t pc) {
   if (!isa_difftest_checkregs(ref, pc)) {
-    isa_reg_display();
     IFDEF(CONFIG_IQUEUE, iqueue_dump());
     nemu_state.state = NEMU_ABORT;
     nemu_state.halt_pc = pc;
