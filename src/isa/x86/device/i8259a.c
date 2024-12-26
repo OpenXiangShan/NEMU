e3ebffe2f7929234220d43228fb04b3c3ce06f5a
jiaxiaoyu <xyyy1420@gmail.com>
2024-12-26 10:43:55 +0800

    fix mmio skip diff flag
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/isa/x86/device/i8259a.c
+++ src/isa/x86/device/i8259a.c
@@ -25,6 +25,6 @@ static void i8259a_io_handler(uint32_t offset, int len, bool is_write) {
 
 void init_i8259a() {
   i8259a_base = (void *)new_space(2);
-  add_pio_map("i8259a-master", I8259A_MASTER_PORT, i8259a_base, 2, i8259a_io_handler);
-  add_pio_map("i8259a-slave",  I8259A_SLAVE_PORT,  i8259a_base, 2, i8259a_io_handler);
+  add_pio_map("i8259a-master", I8259A_MASTER_PORT, i8259a_base, 2, MMIO_READ|MMIO_WRITE|MMIO_EXEC, i8259a_io_handler);
+  add_pio_map("i8259a-slave",  I8259A_SLAVE_PORT,  i8259a_base, 2, MMIO_READ|MMIO_WRITE|MMIO_EXEC, i8259a_io_handler);
 }
