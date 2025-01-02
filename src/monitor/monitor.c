65165ab0fb1905b4e8a75345fab27f950dffd3f1
jiaxiaoyu <xyyy1420@gmail.com>
2025-01-02 11:15:01 +0800

    feat(image load): support load image into flash
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/monitor/monitor.c
+++ src/monitor/monitor.c
@@ -40,6 +40,7 @@ bool small_log = false;
 bool fast_log = false;
 static char *diff_so_file = NULL;
 static char *img_file = NULL;
+static char *flash_image = NULL;
 static int batch_mode = false;
 static int difftest_port = 1234;
 char *max_instr = NULL;
@@ -99,7 +100,7 @@ static inline int parse_args(int argc, char *argv[]) {
     {"config-name"        , required_argument, NULL, 'C'},
 
     // restore cpt
-    {"restore"            , no_argument      , NULL, 'c'},
+    {"flash-image"        , required_argument, NULL, 16},
     {"cpt-restorer"       , required_argument, NULL, 'r'},
     {"map-img-as-outcpt"  , no_argument      , NULL, 13},
 
@@ -148,14 +149,14 @@ static inline int parse_args(int argc, char *argv[]) {
       case 'w': workload_name = optarg; break;
       case 'C': config_name = optarg; break;
 
-      case 'c':
-        checkpoint_restoring = true;
-        Log("Restoring from checkpoint");
+      case 16:
+        flash_image = optarg;
         break;
 
       case 'r':
         restorer = optarg;
         break;
+
       case 13: {
         extern bool map_image_as_output_cpt;
         map_image_as_output_cpt = true;
@@ -179,7 +180,8 @@ static inline int parse_args(int argc, char *argv[]) {
       case 'M':
           mem_dump_file = optarg;
           break;
-      case 'A': 
+
+      case 'A':
           #ifdef CONFIG_MEMORY_REGION_ANALYSIS
           Log("Set mem analysis log path %s", optarg);
           memory_region_record_file = optarg;
@@ -259,7 +261,6 @@ static inline int parse_args(int argc, char *argv[]) {
         printf("\t-w,--workload=WORKLOAD  the name of sub_dir of this run in STAT_DIR\n");
         printf("\t-C,--config=CONFIG      running configuration\n");
 
-        printf("\t-c,--restore            restoring from CPT FILE\n");
         printf("\t-r,--cpt-restorer=R     binary of gcpt restorer\n");
 //        printf("\t--map-img-as-outcpt     map to image as output checkpoint, do not truncate it.\n"); //comming back soon
 
@@ -273,6 +274,7 @@ static inline int parse_args(int argc, char *argv[]) {
         printf("\t--checkpoint-format     Specify the checkpoint format('gz' or 'zstd'), default: 'gz'.\n");
 //        printf("\t--map-cpt               map to this file as pmem, which can be treated as a checkpoint.\n"); //comming back soon
 
+        printf("\t--flash-img             image of flash\n");
         printf("\t--simpoint-profile      simpoint profiling\n");
         printf("\t--dont-skip-boot        profiling/checkpoint immediately after boot\n");
         printf("\t--mem_use_record_file   result output file for analyzing the memory use segment\n");
@@ -310,7 +312,6 @@ void init_monitor(int argc, char *argv[]) {
   if (map_image_as_output_cpt) {
     assert(!mapped_cpt_file);
     mapped_cpt_file = img_file;
-    checkpoint_restoring = true;
   }
 
   extern void init_path_manager();
@@ -338,38 +339,15 @@ void init_monitor(int argc, char *argv[]) {
   /* Perform ISA dependent initialization. */
   init_isa();
 
+  /* Initialize devices. */
+  init_device();
+
   int64_t img_size = 0;
-
-  assert(img_file);
-
-  uint8_t* bbl_start = (uint8_t*)get_pmem();
-  img_size = load_img(img_file, "image (checkpoint/bare metal app/bbl) form cmdline", bbl_start, 0);
-
-  if (restorer) {
-    FILE *restore_fp = fopen(restorer, "rb");
-    Assert(restore_fp, "Can not open '%s'", restorer);
-
-    int restore_size = 0;
-    int restore_jmp_inst = 0;
-
-    int ret = fread(&restore_jmp_inst, sizeof(int), 1, restore_fp);
-    assert(ret == 1);
-    assert(restore_jmp_inst != 0);
-
-    ret = fread(&restore_size, sizeof(int), 1, restore_fp);
-    assert(ret == 1);
-    assert(restore_size != 0);
-
-    fclose(restore_fp);
-
-    load_img(restorer, "Gcpt restorer form cmdline", bbl_start, restore_size);
-  }
+  int64_t flash_size = 0;
+  fill_memory(img_file, flash_image, restorer, &img_size, &flash_size);
 
   /* Initialize differential testing. */
   init_difftest(diff_so_file, img_size, difftest_port);
-
-  /* Initialize devices. */
-  init_device();
 
 #endif
 
