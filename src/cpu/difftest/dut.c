8d7436898ef04a1c5523425906d511f1f9e7be29
jiaxiaoyu <xyyy1420@gmail.com>
2025-01-03 18:50:55 +0800

    fix: fix include head file
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- src/cpu/difftest/dut.c
+++ src/cpu/difftest/dut.c
@@ -20,6 +20,7 @@
 #include <memory/paddr.h>
 #include <utils.h>
 #include <difftest.h>
+#include <device/flash.h>
 
 void (*ref_difftest_memcpy)(paddr_t addr, void *buf, size_t n, bool direction) = NULL;
 void (*ref_difftest_regcpy)(void *dut, bool direction) = NULL;
