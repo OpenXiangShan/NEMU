42c928eb39f9de23f09ff43cf4c05445701c9356
jiaxiaoyu <xyyy1420@gmail.com>
2025-01-09 15:01:57 +0800

    feat: add nanopb repo, prepare for protobuf define memlayout
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- scripts/repos.mk
+++ scripts/repos.mk
@@ -2,5 +2,6 @@ RESOURCE_PATH := resource
 
 include $(NEMU_HOME)/scripts/softfloat.mk
 include $(NEMU_HOME)/scripts/LibcheckpointAlpha.mk
+include $(NEMU_HOME)/scripts/nanopb.mk
 
 clean-repos: clean-softfloat clean-libcheckpointalpha
