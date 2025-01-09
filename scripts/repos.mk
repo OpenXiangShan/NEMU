569269e6b798a3ad4523d55f09b6be8f0f6782e8
jiaxiaoyu <xyyy1420@gmail.com>
2025-01-09 14:44:04 +0800

    feat: add Libcheckpoint repo
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- scripts/repos.mk
+++ scripts/repos.mk
@@ -1,6 +1,7 @@
 RESOURCE_PATH := resource
 
 include $(NEMU_HOME)/scripts/softfloat.mk
+include $(NEMU_HOME)/scripts/Libcheckpoint.mk
 include $(NEMU_HOME)/scripts/LibcheckpointAlpha.mk
 
-clean-repos: clean-softfloat clean-libcheckpointalpha
+clean-repos: clean-softfloat clean-libcheckpointalpha clean-libcheckpoint
