04e74a2a55feab54f1e4a148c0135ff17a8f9965
jiaxiaoyu <xyyy1420@gmail.com>
2025-01-13 10:39:02 +0800

    test
    Signed-off-by: jiaxiaoyu <xyyy1420@gmail.com>

--- scripts/LibcheckpointAlpha.mk
+++ scripts/LibcheckpointAlpha.mk
@@ -5,7 +5,7 @@ CPT_BIN = $(CPT_REPO)/build/gcpt.bin
 CPT_MEMLAYOUT_HEADER = $(CPT_REPO)/src/restore_rom_addr.h
 
 ifeq ($(wildcard $(CPT_MEMLAYOUT_HEADER)),)
-  $(shell git clone --depth=1 https://github.com/OpenXiangShan/LibCheckpointAlpha.git $(CPT_REPO))
+  $(shell git clone --depth=1 https://github.com/OpenXiangShan/LibCheckpointAlpha.git -b revert-7-revert-6-position-indenpendent $(CPT_REPO))
 endif
 
 gcpt_restore_bin: $(CPT_BIN)
