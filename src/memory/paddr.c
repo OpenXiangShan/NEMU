895901f752598c4745acd67cc9d2f740697fc954
sinceforYy <1017657683@qq.com>
2025-04-15 11:07:19 +0800

    fix(pma): split pmp, pma check in check_paddr

--- src/memory/paddr.c
+++ src/memory/paddr.c
@@ -211,12 +211,20 @@ void set_pmem(bool pass_pmem_from_dut, uint8_t *_pmem)
 /* Memory accessing interfaces */
 
 bool check_paddr(paddr_t addr, int len, int type, int trap_type, int mode, vaddr_t vaddr) {
-  if (!isa_pmp_check_permission(addr, len, type, mode) || 
-      !isa_pma_check_permission(addr, len, type)) {
+  if (!isa_pmp_check_permission(addr, len, type, mode)) {
     if (trap_type == MEM_TYPE_WRITE) {
       raise_access_fault(EX_SAF, vaddr);
     }else {
       Log("isa pmp check failed");
+      raise_read_access_fault(trap_type, vaddr);
+    }
+    return false;
+  }
+  if (!isa_pma_check_permission(addr, len, type)) {
+    if (trap_type == MEM_TYPE_WRITE) {
+      raise_access_fault(EX_SAF, vaddr);
+    }else {
+      Log("isa pma check failed");
       raise_read_access_fault(trap_type, vaddr);
     }
     return false;
