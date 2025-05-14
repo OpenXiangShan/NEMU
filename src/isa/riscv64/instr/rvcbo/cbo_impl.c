cbc5a65117e744026d89a3d6fbf2a7250cc4f084
Anzooooo <anzoso@outlook.com>
2025-05-14 18:54:43 +0800

    fix(cbo): cbo requires read permission but throws store exception

--- src/isa/riscv64/instr/rvcbo/cbo_impl.c
+++ src/isa/riscv64/instr/rvcbo/cbo_impl.c
@@ -31,10 +31,10 @@
 #define CACHE_BLOCK_OPS          (CACHE_BLOCK_SIZE / CACHE_OP_SPLIT_SIZE)
 
 
-static void paddr_check(paddr_t paddr, vaddr_t vaddr) {
-  check_paddr(paddr, 8, MEM_TYPE_WRITE, MEM_TYPE_WRITE, cpu.mode, vaddr);
+static void paddr_check(paddr_t paddr, vaddr_t vaddr, int type) {
+  check_paddr(paddr, 8, type, MEM_TYPE_WRITE, cpu.mode, vaddr);
   if (!in_pmem(paddr)) {
-    cpu.trapInfo.tval = paddr;
+    cpu.trapInfo.tval = vaddr;
     cpu.amo = false;
     longjmp_exception(EX_SAF);
   }
@@ -50,33 +50,40 @@ static void trigger_check(vaddr_t vaddr){
 #endif // CONFIG_TDATA1_MCONTROL6
 }
 
-static paddr_t translate_and_check(vaddr_t vaddr) {
+static paddr_t translate_and_check(vaddr_t vaddr, int type) {
   Logm("Checking mmu when MMU_DYN");
   trigger_check(vaddr);
-  int mmu_ret = isa_mmu_check(vaddr, 8, MEM_TYPE_WRITE);
+  int mmu_ret = isa_mmu_check(vaddr, 8, type);
   paddr_t paddr = vaddr;
   if (mmu_ret == MMU_TRANSLATE) {
-    paddr_t pg_base = isa_mmu_translate(vaddr, 8, MEM_TYPE_WRITE);
+    paddr_t pg_base = isa_mmu_translate(vaddr, 8, type);
     int ret = pg_base & PAGE_MASK;
     if (ret == MEM_RET_OK) {
       paddr = pg_base | (vaddr & PAGE_MASK);
     }
   }
-  paddr_check(paddr, vaddr);
+  paddr_check(paddr, vaddr, type);
   return paddr;
 }
 
-static void not_translate_check(vaddr_t vaddr) {
+static void not_translate_check(vaddr_t vaddr, int type) {
   trigger_check(vaddr);
-  paddr_check(vaddr, vaddr);
+  paddr_check(vaddr, vaddr, type);
 }
 
+
+static int convert_load_to_store_exception(int ori_cause) {
+  switch (ori_cause) {
+    case EX_LAF: case EX_LPF: case EX_LGPF: return  ori_cause + 2;
+    default: return ori_cause;
+  }
+}
 
 
 void cbo_zero(Decode *s){
   rtlreg_t* addr_p = dsrc1;
 
-  not_translate_check(*addr_p);
+  not_translate_check(*addr_p, MEM_TYPE_WRITE);
 
   rtlreg_t block_addr = *addr_p & ~CACHE_BLOCK_MASK;
   for (uint64_t i = 0; i < CACHE_BLOCK_OPS; i++) {
@@ -87,41 +94,65 @@ void cbo_zero(Decode *s){
 }
 
 void cbo_inval(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
+    rtlreg_t* addr_p = dsrc1;
 
-  not_translate_check(*addr_p);
+    not_translate_check(*addr_p, MEM_TYPE_READ);
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
 
 void cbo_flush(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
+    rtlreg_t *addr_p = dsrc1;
 
-  not_translate_check(*addr_p);
+    not_translate_check(*addr_p, MEM_TYPE_READ);
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
 
 void cbo_clean(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
+    rtlreg_t *addr_p = dsrc1;
 
-  not_translate_check(*addr_p);
+    not_translate_check(*addr_p, MEM_TYPE_READ);
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
 
 /*
  * MMU impl
  */
 void cbo_zero_mmu(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  rtlreg_t *addr_p = dsrc1;
 
-  paddr_t base_addr_p = translate_and_check(*addr_p);
+  paddr_t base_addr_p = translate_and_check(*addr_p, MEM_TYPE_WRITE);
 
-  rtlreg_t  block_addr = base_addr_p & ~CACHE_BLOCK_MASK;
+  rtlreg_t block_addr = base_addr_p & ~CACHE_BLOCK_MASK;
   for (uint64_t i = 0; i < CACHE_BLOCK_OPS; i++) {
     // write zero to block_addr
     rtl_sm(s, rz, &block_addr, 0, CACHE_OP_SPLIT_SIZE, MMU_DIRECT);
@@ -130,28 +161,54 @@ void cbo_zero_mmu(Decode *s){
 }
 
 void cbo_inval_mmu(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
 
-  translate_and_check(*addr_p);
+    rtlreg_t *addr_p = dsrc1;
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    translate_and_check(*addr_p, MEM_TYPE_READ);
+
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
 
 void cbo_flush_mmu(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
+    rtlreg_t *addr_p = dsrc1;
 
-  translate_and_check(*addr_p);
+    translate_and_check(*addr_p, MEM_TYPE_READ);
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
 
 void cbo_clean_mmu(Decode *s){
-  rtlreg_t* addr_p = dsrc1;
+  int cause;
+  PUSH_CONTEXT(&cause);
+  if (cause) {
+    pop_context();
+    longjmp_exception(convert_load_to_store_exception(cause));
+  } else {
 
-  translate_and_check(*addr_p);
+    rtlreg_t *addr_p = dsrc1;
 
-  // do nothing
-  IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    translate_and_check(*addr_p, MEM_TYPE_READ);
+
+    // do nothing
+    IFNDEF(CONFIG_DIFFTEST_REF_NEMU, difftest_skip_dut(1, 2));
+    pop_context();
+  }
 }
