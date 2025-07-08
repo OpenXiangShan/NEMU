3695fe5ca33a0999982c7f4f74ad492e480a667f
Anzooooo <anzoso@outlook.com>
2025-07-08 17:28:05 +0800

    H-AIA-Log-tmp

--- include/debug.h
+++ include/debug.h
@@ -32,16 +32,16 @@
 #endif // CONFIG_SIMPLE_LOG
 
 extern uint64_t g_nr_guest_instr;
-
+extern unsigned int PMEM_HARTID;
+#define LOG_CONT PMEM_HARTID == 3 && g_nr_guest_instr > 390016037
 #define Logf(flag, ...) \
   do { \
-    if (flag == dflag_mem && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
-    if (flag == dflag_translate && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
-    if (flag == dflag_translate && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
-    if (flag == dflag_trace_inst && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
-    if (flag == dflag_trace_inst_dasm  && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
+    if (flag == dflag_mem && LOG_CONT) Log(__VA_ARGS__); \
+    if (flag == dflag_translate && LOG_CONT) Log(__VA_ARGS__); \
+    if (flag == dflag_trace_inst && LOG_CONT) Log(__VA_ARGS__); \
+    if (flag == dflag_trace_inst_dasm  && LOG_CONT) Log(__VA_ARGS__); \
     if (flag == dflag_trace_bb && ISDEF(CONFIG_TRACE_BB)) Log(__VA_ARGS__); \
-    if (flag == dflag_exit && g_nr_guest_instr>701880000) Log(__VA_ARGS__); \
+    if (flag == dflag_exit && LOG_CONT) Log(__VA_ARGS__); \
     if (flag == dflag_simpoint && ISDEF(CONFIG_SIMPOINT_LOG)) Log(__VA_ARGS__); \
   } while (0)
 
