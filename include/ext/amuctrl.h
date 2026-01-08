96d752c2dc1263c052cf09994eac7b009b1c3be1
yu-yake2002 <yuyake02@outlook.com>
2026-01-08 11:07:48 +0800

    fix(difftest): Fix basic AME difftest

--- include/ext/amuctrl.h
+++ include/ext/amuctrl.h
@@ -10,17 +10,18 @@
 typedef struct  {
   uint8_t  valid;
   uint8_t  op;
+  uint8_t  rm;
   uint8_t  md;       // also used as ms
   uint8_t  sat;      // also used as ls
-  uint8_t  isfp;     // also used as transpose
-  uint8_t  issigned; // also used as isacc
   uint8_t  ms1;
   uint8_t  ms2;
   uint16_t mtilem;   // also used as row
   uint16_t mtilen;   // also used as column
   uint16_t mtilek;
-  uint8_t  types;
-  uint8_t  typed;
+  uint8_t  types1;   // also used as isacc
+  uint8_t  types2;
+  uint8_t  typed;    // also used as widths
+  uint8_t  isfp;     // also used as transpose
   uint64_t base;
   uint64_t stride;
   uint64_t pc;
