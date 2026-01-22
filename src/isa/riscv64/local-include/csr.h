941c6ebdca809189cd3b223f47634cc129114d6d
Anzo <126764519+Anzooooo@users.noreply.github.com>
2026-01-22 19:08:59 +0800

    feat(Custom): SMBLOCKCTL Custom CSR implementation for timeout (#980)

--- src/isa/riscv64/local-include/csr.h
+++ src/isa/riscv64/local-include/csr.h
@@ -45,7 +45,7 @@
 
 #define CUSTOM_CSR_SPFCTL_WMASK     0x3fffff
 #define CUSTOM_CSR_SLVPREDCTL_WMASK 0x1ff
-#define CUSTOM_CSR_SMBLOCKCTL_WMASK 0x3ff
+#define CUSTOM_CSR_SMBLOCKCTL_WMASK 0xffffffff
 #define CUSTOM_CSR_SRNCTL_WMASK     0x5
 #define CUSTOM_CSR_MCOREPWR_WMASK   0x1
 #define CUSTOM_CSR_MFLUSHPWR_WMASK  0x1
@@ -1291,6 +1291,7 @@ CSR_STRUCT_START(smblockctl)
   uint64_t uncache_write_outstanding_enable : 1; // [7] Enable uncache write outstanding (0).
   uint64_t hd_misalign_st_enable            : 1; // [8] Enable hardware store misalign.
   uint64_t hd_misalign_ld_enable            : 1; // [9] Enable hardware load misalign.
+    uint64_t sbuffer_timeout                  : 22;// [31:20] Store buffer flush timeout.
 CSR_STRUCT_END(smblockctl)
 
 /** Supervisor Timer Register**/
