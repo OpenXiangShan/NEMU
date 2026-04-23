259c5bc123a1827a65df568fb725b96a5967929c
Yinan Xu <xuyinan@ict.ac.cn>
2026-04-23 10:27:39 +0800

    fix(device): support dw-apb-uart probing and uart16550 regshift (#1005)
    Expand the uart16550 MMIO window and emulate the minimal
    DesignWare APB UART registers required by Linux probing,
    including USR, RE_EN, DLF, CPR, and UCV.
    
    Decode the classic 16550 register block from aligned byte
    offsets so it works with both regshift 0 and regshift 2
    without aliasing the DesignWare extension registers.
    
    Keep regshift selection as a 0/2 Kconfig choice, derive the
    shift in C, keep FCR state separate from IIR readback, and
    keep uart16550 behavior independent of CONFIG_SHARE while the
    default SHARE configs leave the device disabled.

--- src/device/uart16550.c
+++ src/device/uart16550.c
@@ -17,6 +17,7 @@
 
 #include <utils.h>
 #include <device/map.h>
+#include <memory/host.h>
 
 #define UART_RBR 0x0
 #define UART_THR 0x0
@@ -31,16 +32,43 @@
 #define UART_MSR 0x6
 #define UART_SCR 0x7
 
-#define UART_REG_OFFSET(reg) ((reg) << 2)
+#define UART_STANDARD_REG_NUM 8
+#if defined(CONFIG_UART16550_REG_SHIFT_0)
+#define UART_REG_SHIFT 0
+#elif defined(CONFIG_UART16550_REG_SHIFT_2)
+#define UART_REG_SHIFT 2
+#else
+#error "uart16550 regshift choice is not configured"
+#endif
+#define UART_REG_STRIDE (1u << UART_REG_SHIFT)
+#define UART_REG_OFFSET(reg) ((reg) << UART_REG_SHIFT)
+
+#if UART_REG_SHIFT != 0 && UART_REG_SHIFT != 2
+#error "uart16550 only supports regshift 0 or 2"
+#endif
+
+#define UART_DW_USR     0x7c
+#define UART_DW_DMASA   0xa8
+#define UART_DW_TCR     0xac
+#define UART_DW_DE_EN   0xb0
+#define UART_DW_RE_EN   0xb4
+#define UART_DW_DLF     0xc0
+#define UART_DW_RAR     0xc4
+#define UART_DW_TAR     0xc8
+#define UART_DW_LCR_EXT 0xcc
+#define UART_DW_CPR     0xf4
+#define UART_DW_UCV     0xf8
+
+#define UART16550_PIO_SIZE  0x20
+#define UART16550_MMIO_SIZE 0x1000
 
 #define UART_LCR_DLAB 0x80
 
 #define UART_IIR_NO_INT 0x01
 #define UART_IIR_FE 0xC0
 
-#define UART_FCR_FE 0x01
+#define UART_FCR_FE  0x01
 #define UART_FCR_RFR 0x02
-#define UART_FCR_XFR 0x04
 
 #define UART_LSR_DR   0x01
 #define UART_LSR_THRE 0x20
@@ -50,15 +78,24 @@
 #define UART_MSR_DSR 0x20
 #define UART_MSR_CTS 0x10
 
+#define UART_DW_USR_TFNF 0x02
+#define UART_DW_USR_TFE  0x04
+#define UART_DW_USR_RFNE 0x08
+
+#define UART_DW_DLF_MASK      0x3f
+#define UART_DW_CPR_FIFO_16   (1u << 16)
+#define UART_DW_UCV_DEFAULT   0x3430312aU
+
 static uint8_t *serial_base = NULL;
 static uint16_t divisor = 0x000c; // default to 9600 baud for 115200 base
+static uint8_t fcr = 0;
 
 #ifdef CONFIG_UART16550_INPUT_FIFO
+#include <errno.h>
 #include <fcntl.h>
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <unistd.h>
-#include <errno.h>
 
 #define QUEUE_SIZE 1024
 static char queue[QUEUE_SIZE] = {};
@@ -67,180 +104,252 @@ static int f = 0, r = 0;
 static int fifo_fd = -1;
 
 static void serial_enqueue(char ch) {
-	int next = (r + 1) % QUEUE_SIZE;
-	if (next != f) {
-		queue[r] = ch;
-		r = next;
-	}
+  int next = (r + 1) % QUEUE_SIZE;
+  if (next != f) {
+    queue[r] = ch;
+    r = next;
+  }
 }
 
 static char serial_dequeue() {
-	char ch = 0;
-	if (f != r) {
-		ch = queue[f];
-		f = (f + 1) % QUEUE_SIZE;
-	}
-	return ch;
+  char ch = 0;
+  if (f != r) {
+    ch = queue[f];
+    f = (f + 1) % QUEUE_SIZE;
+  }
+  return ch;
 }
 
 static void serial_poll_input() {
-	if (fifo_fd < 0) {
-		return;
-	}
-	if (f == r) {
-		char input[256];
-		int ret = read(fifo_fd, input, sizeof(input));
-		if (ret > 0) {
-			for (int i = 0; i < ret; i++) {
-				serial_enqueue(input[i]);
-			}
-		}
-	}
+  if (fifo_fd < 0) {
+    return;
+  }
+  if (f == r) {
+    char input[256];
+    int ret = read(fifo_fd, input, sizeof(input));
+    if (ret > 0) {
+      for (int i = 0; i < ret; i++) {
+        serial_enqueue(input[i]);
+      }
+    }
+  }
 }
 
 static void serial_fifo_reset() {
-	f = r = 0;
+  f = r = 0;
 }
 
 static void init_fifo() {
-	int ret = mkfifo(FIFO_PATH, 0666);
-	assert(ret == 0 || errno == EEXIST);
-	fifo_fd = open(FIFO_PATH, O_RDONLY | O_NONBLOCK);
-	assert(fifo_fd != -1);
+  int ret = mkfifo(FIFO_PATH, 0666);
+  assert(ret == 0 || errno == EEXIST);
+  fifo_fd = open(FIFO_PATH, O_RDONLY | O_NONBLOCK);
+  assert(fifo_fd != -1);
 }
 
 #endif
 
-static inline void serial_update_lsr() {
-	serial_base[UART_REG_OFFSET(UART_LSR)] |= UART_LSR_THRE | UART_LSR_TEMT;
-#ifdef CONFIG_UART16550_INPUT_FIFO
-	serial_poll_input();
-	if (f != r) {
-		serial_base[UART_REG_OFFSET(UART_LSR)] |= UART_LSR_DR;
-	} else {
-		serial_base[UART_REG_OFFSET(UART_LSR)] &= ~UART_LSR_DR;
-	}
-#else
-	serial_base[UART_REG_OFFSET(UART_LSR)] &= ~UART_LSR_DR;
-#endif
+static inline uint8_t serial_read8(uint32_t offset) {
+  return serial_base[offset];
 }
 
-static void serial_handle_access(uint32_t offset, int len, bool is_write) {
-	uint32_t reg = (offset >> 2) & 0x7; // reg-shift = 2
-	switch (reg) {
-		case UART_THR:
-			if (serial_base[UART_REG_OFFSET(UART_LCR)] & UART_LCR_DLAB) {
-				if (is_write) {
-					divisor = (divisor & 0xff00) | serial_base[UART_REG_OFFSET(UART_DLL)];
-				} else {
-					serial_base[UART_REG_OFFSET(UART_DLL)] = (uint8_t)(divisor & 0xff);
-				}
-			} else {
-				if (is_write) {
-					#ifndef CONFIG_SHARE
-					putc(serial_base[UART_REG_OFFSET(UART_THR)], stderr);
-					#endif // CONFIG_SHARE
-					serial_update_lsr();
-				} else {
+static inline void serial_write8(uint32_t offset, uint8_t value) {
+  serial_base[offset] = value;
+}
+
+static inline uint32_t serial_read32(uint32_t offset) {
+  return host_read(serial_base + offset, 4);
+}
+
+static inline void serial_write32(uint32_t offset, uint32_t value) {
+  host_write(serial_base + offset, 4, value);
+}
+
+static inline bool serial_decode_standard_reg(uint32_t offset, uint32_t *reg) {
+  if ((offset & (UART_REG_STRIDE - 1)) != 0) {
+    return false;
+  }
+
+  uint32_t shifted_reg = offset >> UART_REG_SHIFT;
+  if (shifted_reg < UART_STANDARD_REG_NUM) {
+    *reg = shifted_reg;
+    return true;
+  }
+
+  return false;
+}
+
+static inline void serial_update_status() {
+  uint8_t lsr = UART_LSR_THRE | UART_LSR_TEMT;
+  uint32_t usr = UART_DW_USR_TFNF | UART_DW_USR_TFE;
+
 #ifdef CONFIG_UART16550_INPUT_FIFO
-					serial_poll_input();
-					if (f != r) {
-						serial_base[UART_REG_OFFSET(UART_RBR)] = (uint8_t)serial_dequeue();
-					} else {
-						serial_base[UART_REG_OFFSET(UART_RBR)] = 0;
-					}
+  serial_poll_input();
+  if (f != r) {
+    lsr |= UART_LSR_DR;
+    usr |= UART_DW_USR_RFNE;
+  }
+#endif
+
+  serial_write8(UART_REG_OFFSET(UART_LSR), lsr);
+  serial_write32(UART_DW_USR, usr);
+}
+
+static void serial_handle_standard_access(uint32_t reg, bool is_write) {
+  switch (reg) {
+    case UART_THR:
+      if (serial_read8(UART_REG_OFFSET(UART_LCR)) & UART_LCR_DLAB) {
+        if (is_write) {
+          divisor = (divisor & 0xff00) | serial_read8(UART_REG_OFFSET(UART_DLL));
+        } else {
+          serial_write8(UART_REG_OFFSET(UART_DLL), (uint8_t)(divisor & 0xff));
+        }
+      } else if (is_write) {
+        putc(serial_read8(UART_REG_OFFSET(UART_THR)), stderr);
+      } else {
+#ifdef CONFIG_UART16550_INPUT_FIFO
+        serial_poll_input();
+        if (f != r) {
+          serial_write8(UART_REG_OFFSET(UART_RBR), (uint8_t)serial_dequeue());
+        } else {
+          serial_write8(UART_REG_OFFSET(UART_RBR), 0);
+        }
 #else
-					serial_base[UART_REG_OFFSET(UART_RBR)] = 0;
+        serial_write8(UART_REG_OFFSET(UART_RBR), 0);
 #endif
-					serial_update_lsr();
-				}
-			}
-			break;
+      }
+      serial_update_status();
+      break;
 
-		case UART_IER:
-			if (serial_base[UART_REG_OFFSET(UART_LCR)] & UART_LCR_DLAB) {
-				if (is_write) {
-					divisor = (divisor & 0x00ff) | ((uint16_t)serial_base[UART_REG_OFFSET(UART_DLH)] << 8);
-				} else {
-					serial_base[UART_REG_OFFSET(UART_DLH)] = (uint8_t)(divisor >> 8);
-				}
-			} else {
-				if (is_write) {
-					serial_base[UART_REG_OFFSET(UART_IER)] &= 0x0f;
-				}
-			}
-			break;
+    case UART_IER:
+      if (serial_read8(UART_REG_OFFSET(UART_LCR)) & UART_LCR_DLAB) {
+        if (is_write) {
+          divisor = (divisor & 0x00ff) |
+                    ((uint16_t)serial_read8(UART_REG_OFFSET(UART_DLH)) << 8);
+        } else {
+          serial_write8(UART_REG_OFFSET(UART_DLH), (uint8_t)(divisor >> 8));
+        }
+      } else if (is_write) {
+        serial_write8(UART_REG_OFFSET(UART_IER),
+                      serial_read8(UART_REG_OFFSET(UART_IER)) & 0x0f);
+      }
+      break;
 
-		case UART_IIR:
-			if (is_write) {
-				uint8_t val = serial_base[UART_REG_OFFSET(UART_FCR)];
-				serial_base[UART_REG_OFFSET(UART_FCR)] = val;
+    case UART_IIR:
+      if (is_write) {
+        uint8_t val = serial_read8(UART_REG_OFFSET(UART_FCR));
+        fcr = val;
 #ifdef CONFIG_UART16550_INPUT_FIFO
-				if (val & UART_FCR_RFR) {
-					serial_fifo_reset();
-				}
+        if (val & UART_FCR_RFR) {
+          serial_fifo_reset();
+        }
 #endif
-				serial_update_lsr();
-			} else {
-				uint8_t iir = UART_IIR_NO_INT;
-				if (serial_base[UART_REG_OFFSET(UART_FCR)] & UART_FCR_FE) {
-					iir |= UART_IIR_FE;
-				}
-				serial_base[UART_REG_OFFSET(UART_IIR)] = iir;
-			}
-			break;
+        serial_update_status();
+      } else {
+        uint8_t iir = UART_IIR_NO_INT;
+        if (fcr & UART_FCR_FE) {
+          iir |= UART_IIR_FE;
+        }
+        serial_write8(UART_REG_OFFSET(UART_IIR), iir);
+      }
+      break;
 
-		case UART_LCR:
-			if (!is_write) {
-				// no side effects
-			}
-			break;
+    case UART_LCR:
+    case UART_MCR:
+      break;
 
-		case UART_MCR:
-			if (!is_write) {
-				// no side effects
-			}
-			break;
+    case UART_LSR:
+      if (!is_write) {
+        serial_update_status();
+      }
+      break;
 
-		case UART_LSR:
-			if (!is_write) {
-				serial_update_lsr();
-			}
-			break;
+    case UART_MSR:
+      if (!is_write) {
+        serial_write8(UART_REG_OFFSET(UART_MSR), UART_MSR_DCD | UART_MSR_DSR | UART_MSR_CTS);
+      }
+      break;
 
-		case UART_MSR:
-			if (!is_write) {
-				// fixed modem status
-				serial_base[UART_REG_OFFSET(UART_MSR)] = UART_MSR_DCD | UART_MSR_DSR | UART_MSR_CTS;
-			}
-			break;
+    case UART_SCR:
+      break;
 
-		case UART_SCR:
-			// read/write scratch
-			break;
+    default:
+      break;
+  }
+}
 
-		default:
-			break;
-	}
+static void serial_handle_dw_access(uint32_t offset, bool is_write) {
+  switch (offset) {
+    case UART_DW_USR:
+      if (!is_write) {
+        serial_update_status();
+      }
+      break;
+
+    case UART_DW_DLF:
+      if (is_write) {
+        serial_write32(UART_DW_DLF, serial_read32(UART_DW_DLF) & UART_DW_DLF_MASK);
+      }
+      break;
+
+    case UART_DW_CPR:
+      if (!is_write) {
+        serial_write32(UART_DW_CPR, UART_DW_CPR_FIFO_16);
+      }
+      break;
+
+    case UART_DW_UCV:
+      if (!is_write) {
+        serial_write32(UART_DW_UCV, UART_DW_UCV_DEFAULT);
+      }
+      break;
+
+    case UART_DW_DMASA:
+    case UART_DW_TCR:
+    case UART_DW_DE_EN:
+    case UART_DW_RE_EN:
+    case UART_DW_RAR:
+    case UART_DW_TAR:
+    case UART_DW_LCR_EXT:
+      break;
+
+    default:
+      break;
+  }
 }
 
 static void serial_io_handler(uint32_t offset, int len, bool is_write) {
-	assert(len >= 1 && len <= 8);
-	serial_handle_access(offset, len, is_write);
+  uint32_t reg = 0;
+
+  assert(len >= 1 && len <= 8);
+  if (serial_decode_standard_reg(offset, &reg)) {
+    serial_handle_standard_access(reg, is_write);
+    return;
+  }
+  serial_handle_dw_access(offset, is_write);
 }
 
 void init_uart16550() {
-	serial_base = new_space(0x20);
-	serial_base[UART_REG_OFFSET(UART_LSR)] = UART_LSR_THRE | UART_LSR_TEMT;
-	serial_base[UART_REG_OFFSET(UART_IIR)] = UART_IIR_NO_INT;
-	serial_base[UART_REG_OFFSET(UART_MSR)] = UART_MSR_DCD | UART_MSR_DSR | UART_MSR_CTS;
+  serial_base = new_space(UART16550_MMIO_SIZE);
+  fcr = 0;
+
+  serial_write8(UART_REG_OFFSET(UART_IIR), UART_IIR_NO_INT);
+  serial_write8(UART_REG_OFFSET(UART_MSR), UART_MSR_DCD | UART_MSR_DSR | UART_MSR_CTS);
+  serial_write32(UART_DW_DLF, 0);
+  serial_write32(UART_DW_DMASA, 0);
+  serial_write32(UART_DW_TCR, 0);
+  serial_write32(UART_DW_DE_EN, 0);
+  serial_write32(UART_DW_RE_EN, 0);
+  serial_write32(UART_DW_RAR, 0);
+  serial_write32(UART_DW_TAR, 0);
+  serial_write32(UART_DW_LCR_EXT, 0);
+  serial_update_status();
 
 #ifdef CONFIG_UART16550_PORT
-	add_pio_map("uart16550", CONFIG_UART16550_PORT, serial_base, 0x20, serial_io_handler);
+  add_pio_map("uart16550", CONFIG_UART16550_PORT, serial_base, UART16550_PIO_SIZE, serial_io_handler);
 #endif
-	add_mmio_map("uart16550", CONFIG_UART16550_MMIO, serial_base, 0x20, serial_io_handler);
+  add_mmio_map("uart16550", CONFIG_UART16550_MMIO, serial_base, UART16550_MMIO_SIZE, serial_io_handler);
 
 #ifdef CONFIG_UART16550_INPUT_FIFO
-	init_fifo();
+  init_fifo();
 #endif
 }
