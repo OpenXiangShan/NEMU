8611e735add1e2c6e3b83f620db6eb16cfcbc18f
Yinan Xu <xuyinan@ict.ac.cn>
2026-04-30 15:12:26 +0800

    fix(uart): display when SHARE is defined (#1019)
    When the user enables the UART device, the UART is expected to display
    the characters.
    
    UART is expected to be disabled in SHARE mode as the REF should not
    handle any MMIO requests.

--- src/device/uartlite.c
+++ src/device/uartlite.c
@@ -125,9 +125,7 @@ static void serial_io_handler(uint32_t offset, int len, bool is_write) {
     /* We bind the serial port with the host stdout in NEMU. */
     case UARTLITE_TX_FIFO:
       if (is_write) {
-	  #ifndef CONFIG_SHARE
           putc(serial_base[UARTLITE_TX_FIFO], stderr);
-          #endif // CONFIG_SHARE
       }
       else panic("Cannot read UARTLITE_TX_FIFO");
       break;
