c7d9e5124eb4b938fa419d11628b20c10991685b
yu-yake2002 <yuyake02@outlook.com>
2026-06-01 17:31:43 +0800

    fix(mma): Update mma to xsai proposal 12

--- src/ext/cutest.c
+++ src/ext/cutest.c
@@ -7,23 +7,23 @@
 extern void (*amu_ctrl_callback_)(amu_ctrl_event_t);
 extern Decode *prev_s;
 
-void cutest_mma_emplace(uint8_t md, bool sat, bool isfp, bool issigned,
+void cutest_mma_emplace(uint8_t md, bool sat, bool isfp,
                                 uint8_t ms1, uint8_t ms2, uint16_t mtilem,
-                                uint16_t mtilen, uint16_t mtilek, uint8_t types,
-                                uint8_t typed) {
-  amu_ctrl_event_t event;
+                                uint16_t mtilen, uint16_t mtilek, uint8_t types1,
+                                uint8_t types2, uint8_t typed) {
+  amu_ctrl_event_t event = {0};
   event.valid = true;
   event.op = 0;
   event.md = md;
   event.sat = sat;
   event.isfp = isfp;
-  event.issigned = issigned;
   event.ms1 = ms1;
   event.ms2 = ms2;
   event.mtilem = mtilem;
   event.mtilen = mtilen;
   event.mtilek = mtilek;
-  event.types = types;
+  event.types1 = types1;
+  event.types2 = types2;
   event.typed = typed;
   event.pc = prev_s->pc;
   amu_ctrl_callback_(event);
@@ -32,7 +32,7 @@ void cutest_mma_emplace(uint8_t md, bool sat, bool isfp, bool issigned,
 void cutest_mls_emplace(uint8_t ms, bool ls, bool transpose, bool isacc,
                                 bool isA, uint64_t base, uint64_t stride, uint16_t row,
                                 uint16_t column, uint8_t msew) {
-  amu_ctrl_event_t event;
+  amu_ctrl_event_t event = {0};
   event.valid = true;
   event.op = 1;
   event.md = ms;
@@ -40,16 +40,18 @@ void cutest_mls_emplace(uint8_t ms, bool ls, bool transpose, bool isacc,
   event.base = base;
   event.stride = stride;
   event.isfp = transpose;
-  event.issigned = isacc;
+  event.types1 = isacc;
+  event.types2 = isA;
   event.mtilem = row;
   event.mtilen = column;
-  event.types = msew;
+  event.typed = msew;
   event.pc = prev_s->pc;
   amu_ctrl_callback_(event);
 }
 
 void cutest_mrelease_emplace(uint8_t msyncRd) {
-  amu_ctrl_event_t event;
+  amu_ctrl_event_t event = {0};
+  event.valid = true;
   event.op = 2;
   event.mtilem = msyncRd;
   event.pc = prev_s->pc;
@@ -57,7 +59,8 @@ void cutest_mrelease_emplace(uint8_t msyncRd) {
 }
 
 void cutest_mzero_emplace(bool isacc, uint8_t md) {
-  amu_ctrl_event_t event;
+  amu_ctrl_event_t event = {0};
+  event.valid = true;
   event.op = 3;
   event.md = md;
   if (isacc) {
